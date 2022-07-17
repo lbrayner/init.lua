9a5c2c53235eea5f3f69b8a43846c79be4291766
lbrayner <lbrayner@users.noreply.github.com>
2022-07-17 12:55:52 -0300

    Improve code in & added comments to diagnostic.lua

--- lua/diagnostic.lua
+++ lua/diagnostic.lua
@@ -1,15 +1,18 @@
 local api = vim.api
 
 local function open_float()
+    -- Save the current cursor position
     local line_col = api.nvim_win_get_cursor(0)
+    -- Move the cursor to the beginning of the line
     api.nvim_win_set_cursor(0,{ line_col[1], 0 })
     local next_pos = vim.diagnostic.get_next_pos()
+    -- If there's no next diagnostic on the current line, there might be one on
+    -- column 1
     if not next_pos or next_pos[1]+1 ~= line_col[1] then
-        if not vim.diagnostic.open_float() then
-            api.nvim_win_set_cursor(0, line_col)
-        end
-        return
+        -- If there isn't, restore the cursor
+        return vim.diagnostic.open_float() or api.nvim_win_set_cursor(0, line_col)
     end
+    -- Move the cursor to the first diagnostic on the line
     api.nvim_win_set_cursor(0, { line_col[1], next_pos[2] })
     local _, win_id = vim.diagnostic.open_float({ close_events={} })
     -- The following snippet is adapted from $VIMRUNTIME/lua/vim/lsp/util.lua
