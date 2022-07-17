8f4f9dac7f434575ad955145b45ff57946a960bb
lbrayner <lbrayner@users.noreply.github.com>
2022-07-17 02:34:48 -0300

    diagnostic.lua: fixed closing floating window problem
    Deferring the creation of the autocmd that closes the window is the
    solution.

--- lua/diagnostic.lua
+++ lua/diagnostic.lua
@@ -9,8 +9,25 @@ local function open_float()
         return vim.diagnostic.open_float()
     end
     api.nvim_win_set_cursor(0, { line_col[1], next_pos[2] })
-    local float_bufnr, _ = vim.diagnostic.open_float({ close_events={} })
-    api.nvim_command(vim.fn.bufwinnr(float_bufnr) .. "wincmd w")
+    local _, win_id = vim.diagnostic.open_float({ close_events={} })
+    -- The following snippet is adapted from $VIMRUNTIME/lua/vim/lsp/util.lua
+    local augroup_name = "preview_window_" .. win_id
+    local augroup = api.nvim_create_augroup(augroup_name, {
+        clear = true,
+    })
+    local create_autocmd = function()
+        api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI", "InsertCharPre" }, {
+            buffer = api.nvim_get_current_buf(),
+            group = augroup,
+            callback = function()
+                api.nvim_del_augroup_by_name(augroup_name)
+                api.nvim_win_close(win_id, true)
+            end,
+        })
+    end
+    -- Deferring the creation of the autocommand because nvim_win_set_cursor
+    -- triggers CursorMoved
+    vim.defer_fn(create_autocmd, 500)
 end
 
 local opts = { noremap=true, silent=true }
