205cb7d161aad62556f1c5c8445e611b49cd4a35
lbrayner <lbrayner@users.noreply.github.com>
2022-07-16 21:43:26 -0300

    nvim_win_set_cursor triggers CursorMoved late
    Window opened by vim.diagnostic.open_float is closed immediately.

--- lua/diagnostic.lua
+++ lua/diagnostic.lua
@@ -1,33 +1,24 @@
-local opts = { noremap=true, silent=true }
+local api = vim.api
 
-local get_line_col = function()
-    local pos = vim.fn.getpos(".")
-    return {pos[2]-1, pos[3]-1}
-end
-
-local open_float = function()
-    local prev_pos = vim.diagnostic.get_prev_pos()
+local function open_float()
+    local line_col = api.nvim_win_get_cursor(0)
     local next_pos = vim.diagnostic.get_next_pos()
-    if not prev_pos and not next_pos then
+    api.nvim_win_set_cursor(0,{ line_col[1], 0 })
+    if not next_pos or next_pos[1]+1 ~= line_col[1] then
+        api.nvim_win_set_cursor(0, line_col)
         return vim.diagnostic.open_float()
     end
-    local line_col = get_line_col()
-    if prev_pos[1] ~= line_col[1] and next_pos[1] ~= line_col[1] then
-        return vim.diagnostic.open_float()
-    end
-    if next_pos[1] == line_col[1] and next_pos[2] > line_col[2] then
-        return vim.diagnostic.goto_next()
-    end
-    if prev_pos[1] == line_col[1] and prev_pos[2] < line_col[2] then
-        return vim.diagnostic.goto_prev()
-    end
+    api.nvim_win_set_cursor(0, { line_col[1], next_pos[2] })
+    local float_bufnr, _ = vim.diagnostic.open_float({ close_events={} })
+    api.nvim_command(vim.fn.bufwinnr(float_bufnr) .. "wincmd w")
 end
+
+local opts = { noremap=true, silent=true }
 
 vim.keymap.set("n", "<space>e",  open_float, opts)
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
 
 -- Requires Neovim 0.7.0+
-local api = vim.api
 api.nvim_create_user_command("DiagnosticSetLocationList",
     vim.diagnostic.setloclist, { nargs = 0 })
