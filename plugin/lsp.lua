cef8cde89773065584d48e99b7a43f79d1d3304c
lbrayner <lbrayner@users.noreply.github.com>
2022-07-20 16:35:40 -0300

    lsp: autostart = false for tsserver

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -1,7 +1,3 @@
-local util = require 'lspconfig.util'
-
-require 'lspconfig'.tsserver.setup {
-    root_dir = function(fname)
-        return util.root_pattern 'tsconfig.json'(fname)
-    end,
+require "lspconfig".tsserver.setup {
+    autostart = false,
 }
