89bf255bc03b25bc2d53e0d8fabfdec41ef11cbd
lbrayner <lbrayner@users.noreply.github.com>
2022-07-23 14:01:57 -0300

    LspFormat & LspWorkspaceFolders

--- lua/lspcommon.lua
+++ lua/lspcommon.lua
@@ -15,24 +15,23 @@ local on_attach = function(client, bufnr)
 
     -- Commands
     vim.api.nvim_buf_create_user_command(bufnr, "LspRename", rename, { nargs="?" })
+    vim.api.nvim_buf_create_user_command(bufnr, "LspFormat", function()
+        vim.lsp.buf.format { async=true }
+    end, { nargs=0 })
+    vim.api.nvim_buf_create_user_command(bufnr, "LspWorkspaceFolders", function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, { nargs=0 })
 
-    -- Mappings.
+    -- Mappings
     local bufopts = { noremap=true, silent=true, buffer=bufnr }
     vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
     vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
     vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-    -- vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
-    -- vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
-    -- vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
-    -- vim.keymap.set("n", "<space>wl", function()
-    --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    -- end, bufopts)
+    -- TODO vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
     vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
-    -- vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
     vim.keymap.set("n", "<F11>", vim.lsp.buf.code_action, bufopts)
     vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-    -- vim.keymap.set("n", "<space>f", vim.lsp.buf.formatting, bufopts)
 end
 
 return {
