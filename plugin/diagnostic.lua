e51096284ca80bb43bd77fd987cad7bfceeedbfe
lbrayner <lbrayner@users.noreply.github.com>
2022-07-29 00:35:48 -0300

    diagnostic: truncate text WIP

--- plugin/diagnostic.lua
+++ plugin/diagnostic.lua
@@ -75,11 +75,6 @@ end
 
 api.nvim_create_user_command("DefaultDiagnostics", DefaultDiagnostics, { nargs=0 })
 
--- For virtual text
-local spacing = 2 -- Even if you set spacing to 0, there are 2 extra spaces
-local prefix = "•"
-local padding = spacing + string.len(prefix) + 2 -- prefix sandwich
-
 local function CustomDiagnostics()
     vim.fn.sign_define(err, { text="Ɛ", texthl=err, linehl="", numhl="" })
     vim.fn.sign_define(war, { text="Ɯ", texthl=war, linehl="", numhl="" })
@@ -87,25 +82,7 @@ local function CustomDiagnostics()
     vim.fn.sign_define(hin, { text="ƕ", texthl=hin, linehl="", numhl="" })
 
     vim.diagnostic.config({ virtual_text={
-        format=function(diagnostic)
-            local message = diagnostic.message
-            local default = string.format(" %s %s", prefix, message)
-            local bufnr = diagnostic.bufnr
-            local winid = vim.fn.bufwinid(bufnr)
-            if winid < 0 then
-                return default
-            end
-            local lnum = diagnostic.lnum
-            local line = api.nvim_buf_get_lines(bufnr, lnum, lnum+1, true)[1]
-            local line_len = string.len(line)
-            local winwidth = api.nvim_win_get_width(winid) - 2 - 3 -- sign & column number
-            local mess_len = string.len(message)
-            if line_len + padding + mess_len > winwidth then
-                return ""
-            end
-            return default
-        end,
-        prefix="",
+        prefix="•",
         spacing=0,
     } })
 end
@@ -113,7 +90,51 @@ end
 api.nvim_create_user_command("CustomDiagnostics", CustomDiagnostics, { nargs=0 })
 
 local augroup = api.nvim_create_augroup("custom_diagnostics", { clear=true })
+
 api.nvim_create_autocmd({ "VimEnter" }, {
     group = augroup,
     callback = CustomDiagnostics,
+})
+
+local function truncate(bufnr, message, lnum)
+    local winid = vim.fn.bufwinid(bufnr)
+    if winid < 0 then
+        return message
+    end
+    local line = api.nvim_buf_get_lines(bufnr, lnum, lnum+1, true)[1]
+    local line_len = string.len(line)
+    local winwidth = api.nvim_win_get_width(winid) - 2 - 3 -- sign & column number
+    local mess_len = string.len(message)
+    if line_len + 1 + mess_len > winwidth then
+        return ""
+    end
+    return message
+end
+
+api.nvim_create_autocmd({ "WinEnter" }, {
+    group = augroup,
+    callback = function()
+        local bufnr = api.nvim_get_current_buf()
+        local clients = vim.lsp.get_active_clients({ bufnr=bufnr })
+        for _, client in ipairs(clients) do
+            local namespace = vim.lsp.diagnostic.get_namespace(client.id)
+            local metadata = vim.diagnostic.get_namespace(namespace)
+            if metadata.user_data and metadata.user_data.virt_text_ns then
+                local virt_text_ns = metadata.user_data.virt_text_ns
+                local extmarks = api.nvim_buf_get_extmarks(bufnr, virt_text_ns, 0, -1, {
+                    details=true })
+                for _, extmark in ipairs(extmarks) do
+                    local id = extmark[1]
+                    local lnum = extmark[2]
+                    local col = extmark[3]
+                    local details = extmark[4]
+                    for _, virt_text_pair in ipairs(details.virt_text) do
+                        virt_text_pair[1] = truncate(bufnr, virt_text_pair[1], lnum)
+                    end
+                    details.id = id
+                    api.nvim_buf_set_extmark(bufnr, virt_text_ns, lnum, col, details)
+                end
+            end
+        end
+    end,
 })
