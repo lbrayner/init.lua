cf8b7173d6b3a862ab78e770a753e8ccf5efc800
lbrayner <lbrayner@users.noreply.github.com>
2022-07-29 22:51:13 -0300

    lsp: trunc_virt_text as a regular autocmd

--- lua/lbrayner/lspcommon.lua
+++ lua/lbrayner/lspcommon.lua
@@ -2,46 +2,6 @@ local keymap = require("lbrayner.keymap")
 local nnoremap = keymap.nnoremap
 local api = vim.api
 
-local function truncate(bufnr, winid, message, lnum)
-    local line = api.nvim_buf_get_lines(bufnr, lnum, lnum+1, true)[1]
-    local line_len = string.len(line)
-    local winwidth = api.nvim_win_get_width(winid) - 2 - 3 -- sign & column number
-    local mess_len = string.len(message)
-    if line_len + 1 + mess_len > winwidth then
-        return ""
-    end
-    return message
-end
-
-local function trunc_virt_text(args)
-    local bufnr = args.buf
-    local winid = vim.fn.bufwinid(bufnr)
-    if winid < 0 then
-        return
-    end
-    local clients = vim.lsp.get_active_clients({ bufnr=bufnr })
-    for _, client in ipairs(clients) do
-        local namespace = vim.lsp.diagnostic.get_namespace(client.id)
-        local metadata = vim.diagnostic.get_namespace(namespace)
-        if metadata.user_data and metadata.user_data.virt_text_ns then
-            local virt_text_ns = metadata.user_data.virt_text_ns
-            local extmarks = api.nvim_buf_get_extmarks(bufnr, virt_text_ns, 0, -1, {
-                details=true })
-            for _, extmark in ipairs(extmarks) do
-                local id = extmark[1]
-                local lnum = extmark[2]
-                local col = extmark[3]
-                local details = extmark[4]
-                for _, virt_text_pair in ipairs(details.virt_text) do
-                    virt_text_pair[1] = truncate(bufnr, winid, virt_text_pair[1], lnum)
-                end
-                details.id = id
-                api.nvim_buf_set_extmark(bufnr, virt_text_ns, lnum, col, details)
-            end
-        end
-    end
-end
-
 -- From nvim-lspconfig
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
@@ -75,14 +35,6 @@ local on_attach = function(client, bufnr)
     nnoremap("<space>D", vim.lsp.buf.type_definition, bufopts)
     nnoremap("<F11>", vim.lsp.buf.code_action, bufopts)
     nnoremap("gr", vim.lsp.buf.references, bufopts)
-
-    -- Autocmds
-    local augroup = api.nvim_create_augroup("trunc_virt_text_" .. bufnr, { clear=true })
-    api.nvim_create_autocmd({ "WinEnter" }, {
-        buffer = bufnr,
-        group = augroup,
-        callback = trunc_virt_text,
-    })
 end
 
 return {
--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -1,4 +1,58 @@
 local lspcommon = require "lbrayner.lspcommon"
+local api = vim.api
+
+local function truncate(bufnr, winid, message, lnum)
+    if string.len(message) == 0 then
+        return message
+    end
+    local line = api.nvim_buf_get_lines(bufnr, lnum, lnum+1, true)[1]
+    local line_len = string.len(line)
+    local winwidth = api.nvim_win_get_width(winid) - 2 - 3 -- sign & column number
+    local mess_len = string.len(message)
+    -- TODO debug
+    print(string.format("bufnr %s lnum %s line_len %s mess_len %s winwidth %s",
+        bufnr, lnum, line_len, mess_len, winwidth))
+    if line_len + 1 + mess_len > winwidth then
+        return ""
+    end
+    return message
+end
+
+local function trunc_virt_text(args)
+    local bufnr = args.buf
+    local winid = vim.fn.bufwinid(bufnr)
+    if winid < 0 then
+        return
+    end
+    local clients = vim.lsp.get_active_clients({ bufnr=bufnr })
+    for _, client in ipairs(clients) do
+        local namespace = vim.lsp.diagnostic.get_namespace(client.id)
+        local metadata = vim.diagnostic.get_namespace(namespace)
+        if metadata.user_data and metadata.user_data.virt_text_ns then
+            local virt_text_ns = metadata.user_data.virt_text_ns
+            local extmarks = api.nvim_buf_get_extmarks(bufnr, virt_text_ns, 0, -1, {
+                details=true })
+            for _, extmark in ipairs(extmarks) do
+                local id = extmark[1]
+                local lnum = extmark[2]
+                local col = extmark[3]
+                local details = extmark[4]
+                for _, virt_text_pair in ipairs(details.virt_text) do
+                    virt_text_pair[1] = truncate(bufnr, winid, virt_text_pair[1], lnum)
+                end
+                details.id = id
+                api.nvim_buf_set_extmark(bufnr, virt_text_ns, lnum, col, details)
+            end
+        end
+    end
+end
+
+-- Autocmds
+local augroup = api.nvim_create_augroup("trunc_virt_text", { clear=true })
+api.nvim_create_autocmd({ "BufWinEnter", "WinEnter" }, {
+    group = augroup,
+    callback = trunc_virt_text,
+})
 
 require "lspconfig".tsserver.setup {
     autostart = false,
