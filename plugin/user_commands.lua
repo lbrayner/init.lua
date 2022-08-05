a0b233b457fe1cea2b5bbafb6c820cdfa95cc086
lbrayner <lbrayner@users.noreply.github.com>
2022-08-05 01:34:41 -0300

    Anonymous lua function

--- plugin/user_commands.lua
+++ plugin/user_commands.lua
@@ -1,7 +1,5 @@
-local function module_reload(command)
+vim.api.nvim_create_user_command("LuaModuleReload", function(command)
     local module = command.args
     package.loaded[module] = nil
     require(module)
-end
-
-vim.api.nvim_create_user_command("LuaModuleReload", module_reload, { bar=true, nargs=1 })
+end, { bar=true, nargs=1 })
