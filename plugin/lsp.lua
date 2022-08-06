02854e785e764a0e4c7a34b1f04bb00aa17c7715
lbrayner <lbrayner@users.noreply.github.com>
2022-08-06 20:26:44 -0300

    lsp.lua: configured pyright for Python

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -1,6 +1,14 @@
 local lspcommon = require "lbrayner.lspcommon"
+local lspconfig = require "lspconfig"
 
-require "lspconfig".tsserver.setup {
+-- Typescript, Javascript
+lspconfig.tsserver.setup {
+    autostart = false,
+    on_attach = lspcommon.on_attach,
+}
+
+-- Python
+lspconfig.pyright.setup {
     autostart = false,
     on_attach = lspcommon.on_attach,
 }
