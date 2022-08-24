27cd3ceb802f44608f11ac126dbb5c8f2f696f73
lbrayner <lbrayner@users.noreply.github.com>
2022-08-23 22:48:02 -0300

    JdtlsStart: respects root_dir

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -7,26 +7,28 @@ vim.api.nvim_buf_create_user_command(0, "JdtlsStart", function(_command)
 
     local jdtls_start = vim.api.nvim_create_augroup("jdtls_start", { clear=true })
 
-    -- TODO check lsp root/workspace dir
-    vim.api.nvim_create_autocmd({ "FileType" }, {
+    vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
         group = jdtls_start,
-        pattern = "java",
+        pattern = config.root_dir .. "/*.java",
         callback = function()
             require("jdtls").start_or_attach(config)
         end,
     })
 
-    -- TODO check lsp root/workspace dir
+    local is_descendant = require("lspconfig.util").path.is_descendant
+
     for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
         if vim.bo.ft == "java" then
-            vim.api.nvim_create_autocmd({ "WinEnter" }, {
-                group = jdtls_start,
-                buffer = bufnr,
-                callback = function()
-                    require("jdtls").start_or_attach(config)
-                    return true -- Delete the autocmd
-                end,
-            })
+            if is_descendant(config.root_dir, vim.api.nvim_buf_get_name(bufnr)) then
+                vim.api.nvim_create_autocmd({ "WinEnter" }, {
+                    group = jdtls_start,
+                    buffer = bufnr,
+                    callback = function()
+                        require("jdtls").start_or_attach(config)
+                        return true -- Delete the autocmd
+                    end,
+                })
+            end
         end
     end
 
