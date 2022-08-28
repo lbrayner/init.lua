d1864223b70ae2ad9896b94560eb9f8a01ab7dbe
lbrayner <lbrayner@users.noreply.github.com>
2022-08-28 18:07:46 -0300

    jdtls.lua: go to class declaration

--- lua/lbrayner/jdtls.lua
+++ lua/lbrayner/jdtls.lua
@@ -1,13 +1,28 @@
 vim.cmd.packadd "nvim-jdtls"
 
-local lspcommon = require "lbrayner.lspcommon"
 local lspconfig = require "lspconfig.server_configurations.jdtls"
+
+local function on_attach(client, bufnr)
+    require "lbrayner.lspcommon".on_attach(client, bufnr)
+
+    -- Override mappings
+    local nnoremap = require("lbrayner.keymap").nnoremap
+    local bufopts = { buffer=bufnr }
+    -- Go to class declaration
+    nnoremap("gD", function()
+        vim.api.nvim_win_set_cursor(0, {1, 0})
+        if vim.fn.search("^public\\s\\+class\\s\\+\\zs" ..
+            vim.fn.expand("%:t:r")) > 0 then
+            vim.cmd "normal! zz"
+        end
+    end, bufopts)
+end
 
 local config = {
     -- The command that starts the language server
     -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
     cmd = lspconfig.default_config.cmd,
-    on_attach = lspcommon.on_attach,
+    on_attach = on_attach,
     root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}),
 }
 
