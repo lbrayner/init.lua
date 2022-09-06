a240da994e834ce12d44b9fab87548404e77a4c0
lbrayner <lbrayner@users.noreply.github.com>
2022-09-06 14:31:09 -0300

    lsp.lua: better name lsp_setup
    Since it pertains to all LSP setups.

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -7,10 +7,10 @@ lspconfig.tsserver.setup {
     on_attach = lspcommon.on_attach,
 }
 
-local lspconfig_setup = vim.api.nvim_create_augroup("lspconfig_setup", { clear=true })
+local lsp_setup = vim.api.nvim_create_augroup("lsp_setup", { clear=true })
 
 vim.api.nvim_create_autocmd("LspDetach", {
-    group = lspconfig_setup,
+    group = lsp_setup,
     desc = "Undo custom statusline",
     callback = function(args)
         vim.b[args.buf].Statusline_custom_rightline = nil
@@ -20,7 +20,7 @@ vim.api.nvim_create_autocmd("LspDetach", {
 
 -- Python
 vim.api.nvim_create_autocmd("LspAttach", {
-    group = lspconfig_setup,
+    group = lsp_setup,
     pattern = { "*.py" },
     desc = "Custom statusline for pyright",
     callback = function(args)
