be1d970adafa3323d3581bb7a8ba29d2affb5a66
lbrayner <lbrayner@users.noreply.github.com>
2022-09-10 14:02:08 -0300

    fzf-lua: suggested fixes to fzf-lua's tabs() command

--- plugin/fzf-lua.lua
+++ plugin/fzf-lua.lua
@@ -24,6 +24,9 @@ fzf.setup {
     files = {
         previewer = false,
     },
+    tabs = {
+        previewer = false,
+    },
 }
 
 local function files()
@@ -40,55 +43,13 @@ local function files_clear_cache()
     vim.cmd.echoerr("'find_file_cache not executable.'")
 end
 
-local function winid_from_tab_buf(tabnr, bufnr)
-    local tabhandle = vim.api.nvim_list_tabpages()[tabnr]
-    for _, w in ipairs(vim.api.nvim_tabpage_list_wins(tabhandle)) do
-        if bufnr == vim.api.nvim_win_get_buf(w) then
-            return w
-        end
-    end
-    return nil
-end
-
-local function focus_on_selected(selected, opts)
-    local tabnr, bufnr = string.match(selected[1], "^%[%s*(%d+)%]%[%s*(%d+)%]")
-    local winid = winid_from_tab_buf(tonumber(tabnr), tonumber(bufnr))
-    if winid then vim.api.nvim_set_current_win(winid) end
-end
-
-local function tabs()
-    local tsize = #tostring(#vim.api.nvim_list_tabpages())
-    local bsize = #tostring(#vim.api.nvim_list_bufs())
-    local size = 1 + tsize + 1 + 1 + bsize + 1 + 1
-    local contents = function(fzf_cb)
-        coroutine.wrap(function()
-            local co = coroutine.running()
-            for i, t in ipairs(vim.api.nvim_list_tabpages()) do
-                for _, w in ipairs(vim.api.nvim_tabpage_list_wins(t)) do
-                    local b = vim.api.nvim_win_get_buf(w)
-                    local name = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(b), ":~")
-                    fzf_cb(string.format("[%"..tsize.."d][%"..bsize.."d] %s", i, b, name),
-                    function() coroutine.resume(co) end)
-                end
-            end
-            fzf_cb()
-        end)()
-    end
-    local opts = {
-        actions = {
-            ["default"] = focus_on_selected,
-        },
-    }
-    fzf.fzf_exec(contents, opts)
-end
-
 nvim_create_user_command("Files", files, { nargs=0 })
 nvim_create_user_command("FilesClearCache", files_clear_cache, { nargs=0 })
-nvim_create_user_command("Tabs", tabs, { nargs=0 })
+nvim_create_user_command("Tabs", fzf.tabs, { nargs=0 })
 
 local opts = { silent=true }
 
 nnoremap("<F5>", fzf.buffers, opts)
 nnoremap("<Leader><F7>", files_clear_cache, opts)
 nnoremap("<F7>", files, opts)
-nnoremap("<F8>", tabs, opts)
+nnoremap("<F8>", fzf.tabs, opts)
