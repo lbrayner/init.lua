1bd0471d84ff46e88d4ece0320601068a9dc1428
lbrayner <lbrayner@users.noreply.github.com>
2022-11-08 09:57:29 -0300

    works

--- lua/lbrayner/tab.lua
+++ lua/lbrayner/tab.lua
@@ -8,16 +8,26 @@ end
 
 local function tab_close_range(from, to)
     local tabs_to_close = {}
+    -- TODO optimize with while
     for i, t in ipairs(vim.api.nvim_list_tabpages()) do
         if i >= from and i <= to then
             table.insert(tabs_to_close, t)
         end
     end
+    -- TODO remove
+    print(vim.inspect(tabs_to_close))
+    local ei = vim.o.eventignore
+    -- TODO remove
+    print(vim.inspect(ei))
+    vim.opt.eventignore:append {"TabClosed"}
     while not vim.tbl_isempty(tabs_to_close) do
         local tab_to_close = table.remove(tabs_to_close, 1)
-        local tabnr = table_find_index_eq(vim.api.nvim_list_tabpages(), tabs_to_close)
+        local tabnr = table_find_index_eq(vim.api.nvim_list_tabpages(), tab_to_close)
         vim.cmd(tabnr .. "tabclose")
     end
+    vim.o.eventignore = ei
+    -- TODO remove
+    print(vim.inspect(vim.o.eventignore))
 end
 
 return {
