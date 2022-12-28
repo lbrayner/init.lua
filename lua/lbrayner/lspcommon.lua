88cd4d5d4a26e154cc3b9b93a9b253981db30ba8
lbrayner <lbrayner@users.noreply.github.com>
2022-12-28 16:49:17 -0300

    lspcommon: reuse_win & commands that don't set it

--- lua/lbrayner/lspcommon.lua
+++ lua/lbrayner/lspcommon.lua
@@ -13,6 +13,21 @@ local function on_attach(client, bufnr)
     -- respects that, so we override it.
     vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
+    -- Mappings
+    local bufopts = { buffer=bufnr }
+    nnoremap("gD", function()
+        vim.lsp.buf.declaration({ reuse_win=true })
+    end, bufopts)
+    nnoremap("gd", function()
+        vim.lsp.buf.definition({ reuse_win=true })
+    end, bufopts)
+    nnoremap("K", vim.lsp.buf.hover, bufopts)
+    nnoremap("gi", vim.lsp.buf.implementation, bufopts)
+    nnoremap("gK", vim.lsp.buf.signature_help, bufopts)
+    nnoremap("<Space>D", vim.lsp.buf.type_definition, bufopts)
+    nnoremap("<F11>", vim.lsp.buf.code_action, bufopts)
+    nnoremap("gr", vim.lsp.buf.references, bufopts)
+
     -- Commands
     nvim_buf_create_user_command(bufnr, "LspRename", function(command)
         local name = command.args
@@ -22,23 +37,14 @@ local function on_attach(client, bufnr)
         vim.lsp.buf.rename()
     end, { nargs="?" })
 
+    nvim_buf_create_user_command(bufnr, "LspDeclaration", vim.lsp.buf.declaration, { nargs=0 })
+    nvim_buf_create_user_command(bufnr, "LspDefinition", vim.lsp.buf.definition, { nargs=0 })
     nvim_buf_create_user_command(bufnr, "LspFormat", function()
         vim.lsp.buf.format { async=true }
     end, { nargs=0 })
     nvim_buf_create_user_command(bufnr, "LspWorkspaceFolders", function()
         print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, { nargs=0 })
-
-    -- Mappings
-    local bufopts = { buffer=bufnr }
-    nnoremap("gD", vim.lsp.buf.declaration, bufopts)
-    nnoremap("gd", vim.lsp.buf.definition, bufopts)
-    nnoremap("K", vim.lsp.buf.hover, bufopts)
-    nnoremap("gi", vim.lsp.buf.implementation, bufopts)
-    nnoremap("gK", vim.lsp.buf.signature_help, bufopts)
-    nnoremap("<Space>D", vim.lsp.buf.type_definition, bufopts)
-    nnoremap("<F11>", vim.lsp.buf.code_action, bufopts)
-    nnoremap("gr", vim.lsp.buf.references, bufopts)
 
     -- Custom statusline
     vim.b[bufnr].Statusline_custom_rightline = '%9*' .. client.name .. '%* '
