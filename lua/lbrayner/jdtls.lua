2bc3b96da49629c84e1df95392fb310bb3e946c2
lbrayner <lbrayner@users.noreply.github.com>
2023-01-24 14:58:13 -0300

    jdtls: no need for local configuration

--- lua/lbrayner/jdtls.lua
+++ lua/lbrayner/jdtls.lua
@@ -2,22 +2,17 @@ vim.cmd.packadd "nvim-jdtls"
 
 local lspconfig = require "lspconfig.server_configurations.jdtls"
 
--- Add this to site local configuration:
-
--- -- jdtls global settings
--- require("lbrayner.jdtls").get_config().settings = {
---     java = {
---         settings = {
---             url = os.getenv("HOME").."/.config/nvim/config/jdtls/settings.prefs",
---         },
---     },
--- }
-
 return {
     get_config = function()
         return {
             cmd = lspconfig.default_config.cmd,
             root_dir = require("jdtls.setup").find_root({".git", "mvnw", "gradlew"}),
+            url = (function()
+                local prefs = os.getenv("HOME").."/.config/nvim/config/jdtls/settings.prefs"
+                if vim.fn.filereadable(prefs) == 1 then
+                    return prefs
+                end
+            end)(),
         }
     end,
 }
