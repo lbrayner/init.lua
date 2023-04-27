0232077186cc7ec5ef27a7e52969c1b25824ce53
lbrayner <lbrayner@users.noreply.github.com>
2023-04-26 21:22:02 -0300

    FlashWindowMode toggles

--- plugin/flash.lua
+++ plugin/flash.lua
@@ -8,7 +8,7 @@ local function restore_winhighlight(winid)
   vim.wo[winid].winhighlight = winhighlight_store[winid]
 end
 
-local function flash_window(winid)
+local function flash_window(winid) -- TODO winid not being used, remove it
   if winid == 0 then
     winid = vim.api.nvim_get_current_win()
   end
@@ -26,12 +26,10 @@ local function flash_window(winid)
     if i % 2 == 0 then
       vim.api.nvim_win_call(winid, function()
         vim.opt.winhighlight:append({ ["Normal"]="DiffAdd" })
-        -- print("append " .. vim.wo[winid].winhighlight) -- TODO remove
       end)
     else
       vim.api.nvim_win_call(winid, function()
         vim.opt.winhighlight:remove({ "Normal" })
-        -- print("remove " .. vim.wo[winid].winhighlight) -- TODO remove
       end)
     end
     if i > 2 then
@@ -45,7 +43,11 @@ end
 local flash_window_mode
 
 vim.api.nvim_create_user_command("FlashWindowMode", function()
-  -- TODO toggle
+  if flash_window_mode then
+    vim.api.nvim_del_augroup_by_id(flash_window_mode)
+    flash_window_mode = nil
+    return print("Flash window mode disabled.")
+  end
   flash_window_mode = vim.api.nvim_create_augroup("flash_window_mode", { clear=true })
   vim.api.nvim_create_autocmd("WinEnter", {
     group = flash_window_mode,
@@ -54,6 +56,7 @@ vim.api.nvim_create_user_command("FlashWindowMode", function()
       flash_window(0)
     end,
   })
+  print("Flash window mode enabled.")
 end, { nargs=0 })
 
 for _, mode in ipairs({ "", -- nvo: normal, visual, operator-pending
