9ddd1a572537648d42ad1e1502be2a09a9e966d4
lbrayner <lbrayner@users.noreply.github.com>
2023-05-05 13:55:23 -0300

    Configured Lua language server

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -10,6 +10,33 @@ require("lspconfig").pyright.setup {
   autostart = false,
 }
 
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#lua_ls
+require("lspconfig").lua_ls.setup {
+  autostart = false,
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you"re using (most
+        -- likely LuaJIT in the case of Neovim)
+        version = "LuaJIT",
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {"vim"},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique
+      -- identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+
 local on_list
 local quickfix_diagnostics_opts = {}
 local lsp_setqflist
