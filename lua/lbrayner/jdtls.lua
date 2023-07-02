a1bce9a46538948b3c084ca3d82f90e94e09a234
lbrayner <lbrayner@users.noreply.github.com>
2023-07-02 14:43:08 -0300

    jdtls.lua: using vim.lsp.protocol.SymbolKind

--- lua/lbrayner/jdtls.lua
+++ lua/lbrayner/jdtls.lua
@@ -22,6 +22,7 @@ function M.get_config()
 end
 
 local offset_encoding = "utf-16"
+local SymbolKind = require("vim.lsp.protocol").SymbolKind
 
 -- Go to top level declaration
 function M.java_go_to_top_level_declaration()
@@ -29,10 +30,11 @@ function M.java_go_to_top_level_declaration()
   vim.lsp.buf_request(0, "textDocument/documentSymbol", params, function(err, result)
     assert(not err, vim.inspect(err))
     local top_level_symbols = vim.tbl_filter(function(symbol)
-      -- org.eclipse.lsp4j.SymbolKind.Class(5)
-      -- org.eclipse.lsp4j.SymbolKind.Enum(10)
-      -- org.eclipse.lsp4j.SymbolKind.Interface(11)
-      return symbol.kind == 5 or symbol.kind == 10 or symbol.kind == 11
+      return vim.tbl_contains({
+        SymbolKind.Class,
+        SymbolKind.Enum,
+        SymbolKind.Interface
+      }, symbol.kind)
     end, result)
     assert(#top_level_symbols == 1, "File contains more than one top level symbol declaration")
     vim.lsp.util.jump_to_location({
