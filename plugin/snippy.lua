660198aa10fa6176313e1c6dc7396bc33e147eec
lbrayner <lbrayner@users.noreply.github.com>
2023-07-02 02:21:28 -0300

    Added and configured nvim-snippy

---
+++ plugin/snippy.lua
@@ -0,0 +1,31 @@
+local mappings = require("snippy.mapping")
+
+-- TODO decide whether to use <Tab> or another key
+vim.keymap.set({ "i", "s" }, "<Tab>", mappings.next("<Tab>"))
+vim.keymap.set({ "i", "s" }, "<S-Tab>", mappings.previous("<S-Tab>"))
+-- TODO analyze the utility of cut_text
+-- vim.keymap.set("x", "<Tab>", mappings.cut_text, { remap = true })
+-- vim.keymap.set("n", "g<Tab>", mappings.cut_text, { remap = true })
+
+local snip_comp_done = vim.api.nvim_create_augroup("snip_comp_done", { clear = true })
+
+vim.api.nvim_create_autocmd("CompleteDone", {
+  group = snip_comp_done,
+  desc = "Snippy LSP snippet expansion in completion items",
+  callback = function(args)
+    local completion_item = vim.tbl_get(vim.v.completed_item, "user_data", "nvim", "lsp", "completion_item")
+    if not completion_item then
+      return
+    end
+    local clients = vim.lsp.get_active_clients()
+    if #clients ~= 1 then
+      return require("snippy").complete_done()
+    end -- TODO request ctx to be in user_data
+    local client = clients[1]
+    local bufnr = args.buf
+    if completion_item.additionalTextEdits then
+      vim.lsp.util.apply_text_edits(completion_item.additionalTextEdits, bufnr, client.offset_encoding)
+    end
+    require("snippy").complete_done()
+  end
+})
