15c904db7c7f4e4be1f40a74fb5da2df7a822646
lbrayner <lbrayner@users.noreply.github.com>
2023-07-24 14:15:16 -0300

    fixing completion for tsserver based on nvim-cmp core

--- plugin/lsp-completion.lua
+++ plugin/lsp-completion.lua
@@ -7,7 +7,6 @@ vim.keymap.set({ "i", "s" }, "<S-Tab>", mappings.previous())
 -- vim.keymap.set("x", "<Tab>", mappings.cut_text, { remap = true })
 -- vim.keymap.set("n", "g<Tab>", mappings.cut_text, { remap = true })
 
-local InsertTextFormat = vim.lsp.protocol.InsertTextFormat
 local complete
 
 local lsp_completion = vim.api.nvim_create_augroup("lsp_completion", { clear = true })
@@ -21,9 +20,6 @@ vim.api.nvim_create_autocmd("CompleteDone", {
       return
     end
     print("completion_item "..vim.inspect(completion_item)) -- TODO debug
-    if completion_item.insertTextFormat ~= InsertTextFormat.Snippet then
-      return
-    end
     local clients = vim.lsp.get_active_clients()
     if #clients ~= 1 then
       return
@@ -43,17 +39,23 @@ vim.api.nvim_create_autocmd("CompleteDone", {
   end
 })
 
+local InsertTextFormat = vim.lsp.protocol.InsertTextFormat
+
 complete = function(client, bufnr, completed_item, completion_item)
   print("completed_item " .. vim.inspect(completed_item)) -- TODO debug
   print("completion_item " .. vim.inspect(completion_item)) -- TODO debug
-  local snippet
+  local is_snippet = completion_item.insertTextFormat ~= InsertTextFormat.Snippet
+  local new_text
   if completion_item.textEdit then
-    snippet = completion_item.textEdit.newText
+    new_text = completion_item.textEdit.newText
     local text_edit = vim.tbl_deep_extend("error", {}, completion_item.textEdit)
-    text_edit.newText = ""
+    if is_snippet then
+      text_edit.newText = ""
+    end
     if text_edit.replace then -- lsp.InsertReplaceEdit
       text_edit.range = text_edit.replace
     end
+    print("new_text "..vim.inspect(new_text)) -- TODO debug
     local text_edits = { text_edit }
     if completion_item.additionalTextEdits then
       for _, text_edit in ipairs(completion_item.additionalTextEdits) do
@@ -62,11 +64,15 @@ complete = function(client, bufnr, completed_item, completion_item)
     end
     vim.lsp.util.apply_text_edits(text_edits, bufnr, client.offset_encoding)
   else
-    snippet = completion_item.insertText or completion_item.textEditText
+    new_text = completion_item.insertText or completion_item.textEditText
     if completion_item.additionalTextEdits then
       vim.lsp.util.apply_text_edits(completion_item.additionalTextEdits, bufnr, client.offset_encoding)
     end
+    if not is_snippet then
+      vim.api.nvim_put({ new_text }, "", false, true)
+    end
   end
-  print("snippet "..vim.inspect(snippet)) -- TODO debug
-  return require("snippy").expand_snippet(snippet)
+  if is_snippet then
+    require("snippy").expand_snippet(new_text)
+  end
 end
