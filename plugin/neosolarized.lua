cc633a28aba7c4cb5b79e58c7b71f0a26193ed6e
lbrayner <lbrayner@users.noreply.github.com>
2023-08-12 01:34:34 -0300

    neosolarized: triggering ColorScheme

--- plugin/neosolarized.lua
+++ plugin/neosolarized.lua
@@ -1,6 +1,17 @@
 require("neosolarized").setup({
-  comment_italics = false,
   background_set = false,
+  comment_italics = false,
 })
 
-vim.cmd("hi QuickFixLine cterm=NONE ctermbg=8 ctermfg=13 guibg=#002b36 guifg=#6c71c4 gui=NONE")
+local neosolarized = vim.api.nvim_create_augroup("neosolarized", { clear = true })
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  group = neosolarized,
+  pattern = "neosolarized",
+  callback = function()
+    vim.cmd("hi QuickFixLine cterm=NONE ctermbg=8 ctermfg=13 guibg=#002b36 guifg=#6c71c4 gui=NONE")
+    vim.wo.cursorline = true
+  end,
+})
+
+vim.cmd("colorscheme neosolarized")
