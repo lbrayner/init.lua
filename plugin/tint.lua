f4b1a6228cb9257e7be4ddc3643d2049ea000ca3
lbrayner <lbrayner@users.noreply.github.com>
2023-08-21 17:06:42 -0300

    tint: ignoring diff and preview windows

--- plugin/tint.lua
+++ plugin/tint.lua
@@ -2,12 +2,13 @@
 require("tint").setup({
   highlight_ignore_patterns = { "^VertSplit", "^WinSeparator", },
   window_ignore_function = function(winid)
+    local diff = vim.wo[winid].diff
+    local preview = vim.wo[winid].previewwindow
     -- TODO modernize this
     local bufid = vim.api.nvim_win_get_buf(winid)
     local buftype = vim.api.nvim_buf_get_option(bufid, "buftype")
     local floating = vim.api.nvim_win_get_config(winid).relative ~= ""
 
-    -- Do not tint  floating windows, or windows with special buffers, tint everything else
-    return buftype ~= "" or floating
+    return diff or preview or buftype ~= "" or floating
   end
 })
