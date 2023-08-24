4e229056ed84846c70bb66155750c5a4f7fb007d
lbrayner <lbrayner@users.noreply.github.com>
2023-08-24 16:14:21 -0300

    dapui commands

--- plugin/dap.lua
+++ plugin/dap.lua
@@ -1,2 +1,14 @@
 -- TODO move to init.lua
 require("dapui").setup()
+
+vim.api.nvim_create_user_command("DapUiClose", function(command)
+  require("dapui").close({ layout = tonumber(command.args) })
+end, { nargs = "?" })
+
+vim.api.nvim_create_user_command("DapUiOpen", function(command)
+  require("dapui").open({ layout = tonumber(command.args) })
+end, { nargs = "?" })
+
+vim.api.nvim_create_user_command("DapUiReset", function(command)
+  require("dapui").setup()
+end, { nargs = 0 })
