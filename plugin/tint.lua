731641bf268fbf7d0f43f9d5727af0896bd9cada
lbrayner <lbrayner@users.noreply.github.com>
2023-08-29 16:07:47 -0300

    tint: untinting ignored windows

--- plugin/tint.lua
+++ plugin/tint.lua
@@ -1,7 +1,6 @@
 -- TODO move to init.lua
 require("tint").setup({
   highlight_ignore_patterns = { "^VertSplit", "^WinSeparator", },
-  dynamic_window_ignore = true,
   window_ignore_function = function(winid)
     local diff = vim.wo[winid].diff
     local preview = vim.wo[winid].previewwindow
@@ -11,4 +10,21 @@ require("tint").setup({
 
     return diff or preview or buftype ~= "" or floating
   end
+})
+
+local tint_custom = vim.api.nvim_create_augroup("tint_custom", { clear = true })
+
+vim.api.nvim_create_autocmd("WinEnter", {
+  group = tint_custom,
+  desc = "Untint ignored windows",
+  callback = function()
+    local winid = vim.api.nvim_get_current_win()
+
+    local diff = vim.wo[winid].diff
+    local preview = vim.wo[winid].previewwindow
+
+    if diff or previewwindow then
+      require("tint").untint(winid)
+    end
+  end,
 })
