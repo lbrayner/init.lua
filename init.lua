1550b49f29a46270402c7ff985ebb8068296656d
lbrayner <lbrayner@users.noreply.github.com>
2023-09-18 11:24:07 -0300

    init.lua

---
+++ init.lua
@@ -0,0 +1,26 @@
+vim.opt.backspace:append({ "indent", "eol", "start" })
+vim.o.backupcopy="yes" -- So that watchprocesses work as expected
+vim.o.breakindent = true
+-- vim.o.completeopt=menuone
+-- vim.o.cursorline
+-- vim.o.expandtab
+-- vim.o.fileformat=unix
+-- vim.o.fileformats=unix,dos
+-- vim.o.ignorecase
+-- vim.o.lazyredraw
+-- vim.o.linebreak
+-- vim.o.listchars=eol:¬,tab:»\ ,trail:·
+-- vim.o.noruler
+-- vim.o.noshowmode
+-- vim.o.number
+-- vim.o.relativenumber
+-- vim.o.shiftwidth=2 " when indenting with '>', use 2 spaces width
+-- vim.o.smartcase
+-- vim.o.splitbelow
+-- vim.o.splitright
+-- vim.o.switchbuf=usetab,uselast
+-- vim.o.synmaxcol=500 " From tpope's vim-sensible (lowering this improves performance in files with long lines)
+-- vim.o.tabstop=4 " show existing tab with 4 spaces width
+-- vim.o.title
+-- vim.o.wildmenu
+-- vim.o.wildmode=longest:full
