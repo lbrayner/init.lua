d9a0f2addc6619290399c65478591f69c579691b
lbrayner <lbrayner@users.noreply.github.com>
2023-09-24 14:25:26 -0300

    Ported until statusline#VersionControl

--- lua/lbrayner/statusline/init.lua
+++ lua/lbrayner/statusline/init.lua
@@ -32,4 +32,32 @@ function M.highlight_diagnostics()
   vim.cmd(string.format("highlight! User7 ctermfg=%s guifg=%s", cterm, gui))
 end
 
+-- TODO is require("lbrayner.diagnostic").buffer_severity() necessary?
+function M.diagnostics()
+  local buffer_severity = require("lbrayner.diagnostic").buffer_severity()
+  if not buffer_severity then
+    return " "
+  end
+  M.highlight_diagnostics()
+  local prefix = require("lbrayner.diagnostic").get_prefix()
+  return "%7*"..prefix.."%*"
+end
+
+function M.version_control()
+  if not vim.fn.exists("*FugitiveHead") then
+    return ""
+  end
+  local branch = vim.fn.FugitiveHead()
+  if branch == "" then
+    branch = vim.fn["fugitive#Head"](7)
+  end
+  if branch == "" then
+    return ""
+  end
+  if string.len(branch) > 30 then
+    return string.sub(branch, 1, 24).."…"..string.sub(branch, -5)
+  end
+  return branch
+end
+
 return M
