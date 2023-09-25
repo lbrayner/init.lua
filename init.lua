6004db40527eb9dfa3315c6377d9e8fc941d96d6
lbrayner <lbrayner@users.noreply.github.com>
2023-09-25 11:17:45 -0300

    Setting global options instead of buffer local

--- init.lua
+++ init.lua
@@ -1,24 +1,28 @@
 -- Subsection: settings {{{
 
-vim.bo.expandtab = true
-vim.bo.fileformat = "unix"
-vim.bo.shiftwidth = 2 -- when indenting with '>', use 2 spaces width
-vim.bo.synmaxcol = 500 -- From tpope's vim-sensible (lowering this improves performance in files with long lines)
-vim.bo.tabstop = 4 -- show existing tab with 4 spaces width
-
 vim.go.backspace =  "indent,eol,start"
 vim.go.backupcopy = "yes" -- So that watchprocesses work as expected
+vim.go.breakindent = true
 vim.go.completeopt = "menuone"
+vim.go.cursorline = true
+vim.go.expandtab = true
+vim.go.fileformat = "unix"
 vim.go.fileformats = "unix,dos"
 vim.go.ignorecase = true
 vim.go.lazyredraw = true
+vim.go.linebreak = true
 vim.go.listchars = "eol:¬,tab:» ,trail:·"
+vim.go.number = true
+vim.go.relativenumber = true
 vim.go.ruler = false
+vim.go.shiftwidth = 2 -- when indenting with '>', use 2 spaces width
 vim.go.showmode = false
 vim.go.smartcase = true
 vim.go.splitbelow = true
 vim.go.splitright = true
 vim.go.switchbuf = "usetab,uselast"
+vim.go.synmaxcol = 500 -- From tpope's vim-sensible (lowering this improves performance in files with long lines)
+vim.go.tabstop = 4 -- show existing tab with 4 spaces width
 if vim.env.TERM == "foot" or string.find(vim.env.TERM, "256color") then
     vim.go.termguicolors = true
 end
@@ -26,11 +30,6 @@ vim.go.title = true
 vim.go.wildmenu = true
 vim.go.wildmode = "longest:full"
 
-vim.wo.breakindent = true
-vim.wo.cursorline = true
-vim.wo.linebreak = true
-vim.wo.number = true
-vim.wo.relativenumber = true
 -- }}}
 
 -- Subsection: mappings — pt-BR keyboard {{{
