a1e9145ce668d150f31a223d9a8dc6827ce51d7a
lbrayner <lbrayner@users.noreply.github.com>
2023-10-28 14:11:08 -0300

    bundle.lua: using vim.opt

--- bundle.lua
+++ bundle.lua
@@ -1,26 +1,22 @@
 local config = vim.fn.fnamemodify(vim.fn.stdpath("config"), ":h:p")
 local data = vim.fn.fnamemodify(vim.fn.stdpath("data"), ":h:p")
 
-local _config_ = vim.fn.fnameescape(config)
-local _data_ = vim.fn.fnameescape(data)
+vim.opt.runtimepath:remove(string.format("%s/nvim", config))
+vim.opt.runtimepath:remove(string.format("%s/nvim/after", config))
+vim.opt.runtimepath:remove(string.format("%s/nvim/site", data))
+vim.opt.runtimepath:remove(string.format("%s/nvim/site/after", data))
 
-vim.cmd(string.format("set runtimepath-=%s/nvim", _config_))
-vim.cmd(string.format("set runtimepath-=%s/nvim/after", _config_))
-vim.cmd(string.format("set runtimepath-=%s/nvim/site", _data_))
-vim.cmd(string.format("set runtimepath-=%s/nvim/site/after", _data_))
-
-vim.cmd(string.format("set packpath-=%s/nvim", _config_))
-vim.cmd(string.format("set packpath-=%s/nvim/after", _config_))
-vim.cmd(string.format("set packpath-=%s/nvim/site", _data_))
-vim.cmd(string.format("set packpath-=%s/nvim/site/after", _data_))
+vim.opt.packpath:remove(string.format("%s/nvim", config))
+vim.opt.packpath:remove(string.format("%s/nvim/after", config))
+vim.opt.packpath:remove(string.format("%s/nvim/site", data))
+vim.opt.packpath:remove(string.format("%s/nvim/site/after", data))
 
 local vim_dir = vim.fn.expand("<sfile>:p:h")
-local _vim_dir_ = vim.fn.fnameescape(vim_dir)
 
-vim.cmd(string.format("set runtimepath+=%s", _vim_dir_))
-vim.cmd(string.format("set runtimepath+=%s/after", _vim_dir_))
-vim.cmd(string.format("set packpath+=%s", _vim_dir_))
-vim.cmd(string.format("set packpath+=%s/after", _vim_dir_))
+vim.opt.runtimepath:append(string.format("%s", vim_dir))
+vim.opt.runtimepath:append(string.format("%s/after", vim_dir))
+vim.opt.packpath:append(string.format("%s", vim_dir))
+vim.opt.packpath:append(string.format("%s/after", vim_dir))
 
 -- sourcing init.lua
 
