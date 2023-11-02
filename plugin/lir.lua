6c4c205727c8bd5a2ab2fcf7f439b64c135f3a07
lbrayner <lbrayner@users.noreply.github.com>
2023-11-02 10:47:07 -0300

    Disabled MiniFiles, added lir.nvim

---
+++ plugin/lir.lua
@@ -0,0 +1,56 @@
+vim.g.loaded_netrwPlugin = 1
+
+local actions = require("lir.actions")
+local clipboard_actions = require("lir.clipboard.actions")
+local mark_actions = require("lir.mark.actions")
+
+require("lir").setup {
+  show_hidden_files = false,
+  devicons = {
+    enable = true,
+    highlight_dirname = true,
+  },
+  mappings = {
+    ["l"]     = actions.edit,
+    ["<C-s>"] = actions.split,
+    ["<C-v>"] = actions.vsplit,
+    ["<C-t>"] = actions.tabedit,
+
+    ["h"]     = actions.up,
+    ["q"]     = actions.quit,
+
+    ["K"]     = actions.mkdir,
+    ["N"]     = actions.newfile,
+    ["R"]     = actions.rename,
+    ["@"]     = actions.cd,
+    ["Y"]     = actions.yank_path,
+    ["."]     = actions.toggle_show_hidden,
+    ["D"]     = actions.delete,
+
+    ["J"] = function()
+      mark_actions.toggle_mark()
+      vim.cmd("normal! j")
+    end,
+    ["C"] = clipboard_actions.copy,
+    ["X"] = clipboard_actions.cut,
+    ["P"] = clipboard_actions.paste,
+  },
+  hide_cursor = true
+}
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "lir",
+  callback = function()
+    -- use visual mode
+    vim.api.nvim_buf_set_keymap(
+    0,
+    "x",
+    "J",
+    ":<C-u>lua require'lir.mark.actions'.toggle_mark('v')<CR>",
+    { noremap = true, silent = true }
+    )
+
+    -- echo cwd
+    vim.api.nvim_echo({ { vim.fn.expand("%:p"), "Normal" } }, false, {})
+  end
+})
