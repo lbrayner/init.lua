ca6c2b37c689c0a3c36478af04ca02f1eac3489d
lbrayner <lbrayner@users.noreply.github.com>
2023-11-28 09:59:08 -0300

    Ordered items in clipboard module

--- lua/lbrayner/clipboard.lua
+++ lua/lbrayner/clipboard.lua
@@ -1,35 +1,5 @@
 local M = {}
 
-function M.path()
-  if vim.api.nvim_buf_get_name(0) == "" then
-    return ""
-  end
-  if not require("lbrayner").is_in_directory(vim.api.nvim_buf_get_name(0), vim.fn.getcwd(), true) then
-    return M.full_path() -- In case buffer represents a directory
-  end
-  return vim.fn.expand("%:.")
-end
-
-function M.full_path()
-  return vim.fn.expand("%:~")
-end
-
-function M.name()
-  return vim.fn.expand("%:t")
-end
-
-function M.cwd()
-  return vim.fn.fnamemodify(vim.fn.getcwd(), ":~")
-end
-
-function M.directory()
-  return vim.fn.expand("%:~:h")
-end
-
-function M.relative_directory()
-  return vim.fn.expand("%:h")
-end
-
 function M.clip(text)
   if text then
     text = tostring(text)
@@ -40,12 +10,44 @@ function M.clip(text)
   vim.cmd.echo(string.format("'%s'", vim.fn.getreg('"')))
 end
 
+function M.cwd()
+  return vim.fn.fnamemodify(vim.fn.getcwd(), ":~")
+end
+
+function M.directory()
+  return vim.fn.expand("%:~:h")
+end
+
+function M.full_path()
+  return vim.fn.expand("%:~")
+end
+
+function M.name()
+  return vim.fn.expand("%:t")
+end
+
+function M.path()
+  if vim.api.nvim_buf_get_name(0) == "" then
+    return ""
+  end
+  if not require("lbrayner").is_in_directory(vim.api.nvim_buf_get_name(0), vim.fn.getcwd(), true) then
+    return M.full_path() -- In case buffer represents a directory
+  end
+  return vim.fn.expand("%:.")
+end
+
+function M.relative_directory()
+  return vim.fn.expand("%:h")
+end
+
 vim.api.nvim_create_user_command("Clip", function(command)
   M.clip(command.args)
 end, { nargs = "?" })
-
-vim.api.nvim_create_user_command("Path", function()
-  M.clip(M.path())
+vim.api.nvim_create_user_command("Cwd", function()
+  M.clip(M.cwd())
+end, { nargs = 0 })
+vim.api.nvim_create_user_command("Directory", function()
+  M.clip(M.directory())
 end, { nargs = 0 })
 vim.api.nvim_create_user_command("FullPath", function()
   M.clip(M.full_path())
@@ -53,11 +55,8 @@ end, { nargs = 0 })
 vim.api.nvim_create_user_command("Name", function()
   M.clip(M.name())
 end, { nargs = 0 })
-vim.api.nvim_create_user_command("Cwd", function()
-  M.clip(M.cwd())
-end, { nargs = 0 })
-vim.api.nvim_create_user_command("Directory", function()
-  M.clip(M.directory())
+vim.api.nvim_create_user_command("Path", function()
+  M.clip(M.path())
 end, { nargs = 0 })
 vim.api.nvim_create_user_command("RelativeDirectory", function()
   M.clip(M.relative_directory())
