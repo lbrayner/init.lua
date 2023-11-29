21046ab73b3f40af6918057ae3ce0736dced5a3f
lbrayner <lbrayner@users.noreply.github.com>
2023-11-29 06:04:32 -0300

    bundle.lua: vim.fs functions

--- bundle.lua
+++ bundle.lua
@@ -1,26 +1,26 @@
-local config = vim.fn.fnamemodify(vim.fn.stdpath("config"), ":h:p")
-local data = vim.fn.fnamemodify(vim.fn.stdpath("data"), ":h:p")
+local config = vim.fs.normalize(vim.fn.fnamemodify(vim.fn.stdpath("config"), ":p")) -- ~/.config/nvim
+local data = vim.fs.normalize(vim.fn.fnamemodify(vim.fn.stdpath("data"), ":p")) -- ~/.local/share/nvim
 
-vim.opt.runtimepath:remove(string.format("%s/nvim", config))
-vim.opt.runtimepath:remove(string.format("%s/nvim/after", config))
-vim.opt.runtimepath:remove(string.format("%s/nvim/site", data))
-vim.opt.runtimepath:remove(string.format("%s/nvim/site/after", data))
+vim.opt.runtimepath:remove(config)
+vim.opt.runtimepath:remove(vim.fs.joinpath(config, "after"))
+vim.opt.runtimepath:remove(vim.fs.joinpath(data, "site"))
+vim.opt.runtimepath:remove(vim.fs.joinpath(data, "site/after"))
 
-vim.opt.packpath:remove(string.format("%s/nvim", config))
-vim.opt.packpath:remove(string.format("%s/nvim/after", config))
-vim.opt.packpath:remove(string.format("%s/nvim/site", data))
-vim.opt.packpath:remove(string.format("%s/nvim/site/after", data))
+vim.opt.packpath:remove(config)
+vim.opt.packpath:remove(vim.fs.joinpath(config, "after"))
+vim.opt.packpath:remove(vim.fs.joinpath(data, "site"))
+vim.opt.packpath:remove(vim.fs.joinpath(data, "site/after"))
 
 local vim_dir = vim.fn.expand("<sfile>:p:h")
 
 vim.opt.runtimepath:append(vim_dir)
-vim.opt.runtimepath:append(string.format("%s/after", vim_dir))
+vim.opt.runtimepath:append(vim.fs.joinpath(vim_dir, "after"))
 vim.opt.packpath:append(vim_dir)
-vim.opt.packpath:append(string.format("%s/after", vim_dir))
+vim.opt.packpath:append(vim.fs.joinpath(vim_dir, "after"))
 
 -- sourcing init.lua
 
-local init = string.format("%s/init.lua", vim_dir)
+local init = vim.fs.joinpath(vim_dir, "init.lua")
 if vim.fn.filereadable(init) then
   vim.cmd.source(vim.fn.fnameescape(init))
 end
