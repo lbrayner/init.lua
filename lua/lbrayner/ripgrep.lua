053a0f05cabfcc8f599afdd1a55f6aacbb3907da
lbrayner <brayner.l@gmail.com>
2023-12-13 17:49:05 -0300

    ripgrep.lua: RgAgain performs the last search again
    - "!" is part of command.
    - Not using Vim commands echo and echoerr anymore.

--- lua/lbrayner/ripgrep.lua
+++ lua/lbrayner/ripgrep.lua
@@ -3,9 +3,9 @@
 local M = {}
 
 local function rg(txt, loclist)
-  local command = "grep"
+  local command = "grep!"
   if loclist then
-    command = "lgrep"
+    command = "lgrep!"
   end
   local rgopts = " "
   if vim.go.ignorecase then
@@ -21,7 +21,7 @@ local function rg(txt, loclist)
     error("Rg: 'rg' not executable.")
   end
   -- Escaping Command-line special characters '#', '%' (:h :_%), and '|' (:h :bar)
-  vim.cmd("silent "..command.."! "..rgopts..vim.fn.escape(txt, "#%|"))
+  vim.cmd("silent "..command.." "..rgopts..vim.fn.escape(txt, "#%|"))
 end
 
 function M.lrg(txt)
@@ -36,20 +36,18 @@ vim.go.grepprg = "rg --vimgrep"
 vim.go.grepformat = "%f:%l:%c:%m"
 vim.go.shellpipe = "&>"
 
-vim.api.nvim_create_user_command("Rg", function(command)
-  local txt = command.args
-
+local function ripgrep(txt, line1, line2)
   if txt == "" then
     -- https://neovim.discourse.group/t/function-that-return-visually-selected-text/1601
     local pos_start = vim.api.nvim_buf_get_mark(0, "<")
     local pos_end = vim.api.nvim_buf_get_mark(0, ">")
-    if command.line1 ~= pos_start[1] or
-      command.line2 ~= pos_end[1] then
-      vim.cmd.echomsg("'Line range not allowed, only visual selection.'")
+    if line1 ~= pos_start[1] or
+      line2 ~= pos_end[1] then
+      print("Line range not allowed, only visual selection.")
       return
     end
     if pos_start[1] ~= pos_end[1] then
-      vim.cmd.echomsg("'Visual selection pattern cannot span multiple lines.'")
+      print("Visual selection pattern cannot span multiple lines.")
       return
     end
     txt = vim.api.nvim_buf_get_text(0, pos_start[1] - 1, pos_start[2], pos_end[1] - 1, pos_end[2] + 1, {})[1]
@@ -61,21 +59,34 @@ vim.api.nvim_create_user_command("Rg", function(command)
   if not success then
     vim.cmd.cclose()
     if type(err) == "string" and string.find(err, " Rg:") then
-      vim.cmd.echoerr(string.format('"%s"', vim.fn.escape(err, '"')))
-      return
+      error(err)
     end
-    vim.cmd.echomsg(string.format('"Error searching for %s. Unmatched quotes? Check your command."',
-      vim.fn.escape(txt, '"')))
+    print(string.format("Error searching for “%s”. Unmatched quotes? Check your command.", txt))
     return
   end
+
+  vim.fn.setqflist({}, "a", { context = { ripgrep = { txt = txt } } })
 
   if not vim.tbl_isempty(vim.fn.getqflist()) then
     vim.cmd("botright copen")
   else
     vim.cmd.cclose()
-    vim.cmd.echomsg(string.format('"No match found for “%s”."', vim.fn.escape(txt, [["\]])))
+    print(string.format("No match found for “%s”.", txt))
   end
+end
+
+vim.api.nvim_create_user_command("Rg", function(command)
+  ripgrep(command.args, command.line1, command.line2)
 end, { complete = "file", nargs = "*", range = true })
+
+vim.api.nvim_create_user_command("RgAgain", function()
+  local context = vim.fn.getqflist({ context = 1 }).context
+  if vim.tbl_get(context, "ripgrep", "txt") then
+    ripgrep(context.ripgrep.txt)
+  else
+    print("Cannot perform a ripgrep search without context.")
+  end
+end, { nargs = 0 })
 
 vim.keymap.set("ca", "Rg", "Rg -e")
 vim.keymap.set("ca", "Rb", [[Rg -s -e'\b''''\b'<Left><Left><Left><Left><Left>]])
