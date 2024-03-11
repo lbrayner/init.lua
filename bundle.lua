31ca7c67f9c6a70e81aabe22b5b493e45b43d357
lbrayner <brayner.l@gmail.com>
2024-03-11 12:32:34 -0300

    Code review: joinpath accepts multiple arguments

--- bundle.lua
+++ bundle.lua
@@ -4,12 +4,12 @@ local data = vim.fn.stdpath("data") -- ~/.local/share/nvim
 vim.opt.runtimepath:remove(config)
 vim.opt.runtimepath:remove(vim.fs.joinpath(config, "after"))
 vim.opt.runtimepath:remove(vim.fs.joinpath(data, "site"))
-vim.opt.runtimepath:remove(vim.fs.joinpath(data, "site/after"))
+vim.opt.runtimepath:remove(vim.fs.joinpath(data, "site", "after"))
 
 vim.opt.packpath:remove(config)
 vim.opt.packpath:remove(vim.fs.joinpath(config, "after"))
 vim.opt.packpath:remove(vim.fs.joinpath(data, "site"))
-vim.opt.packpath:remove(vim.fs.joinpath(data, "site/after"))
+vim.opt.packpath:remove(vim.fs.joinpath(data, "site", "after"))
 
 local vim_dir = vim.fn.expand("<sfile>:p:h")
 
