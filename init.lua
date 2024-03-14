29c1785c1f5686ea91d158f19092713d4250f6fa
lbrayner <brayner.l@gmail.com>
2024-03-14 11:38:13 -0300

    Move LuaModuleReload to init.lua, commands in alphabetical order

--- init.lua
+++ init.lua
@@ -141,6 +141,43 @@ end
 
 vim.api.nvim_create_user_command("Number", number, { nargs = 0 })
 
+vim.api.nvim_create_user_command("Filter", function(command)
+  local line_start = command.line1
+  local line_end = command.line2
+  local offset = 0
+  for linenr = line_start, line_end do
+    vim.api.nvim_win_set_cursor(0, { linenr + offset, 0 })
+    local output = vim.fn.systemlist(vim.fn.getline(linenr + offset))
+    vim.cmd.delete()
+    vim.fn.append(linenr + offset - 1, output)
+    if not vim.tbl_isempty(output) then
+      offset = offset + #output - 1
+    end
+  end
+  vim.api.nvim_win_set_cursor(0, { line_start, 0 })
+end, { nargs = 0, range = true })
+
+vim.api.nvim_create_user_command("LuaModuleReload", function(command)
+  local module, replacements
+  module = string.gsub(command.args, "^lua/", "")
+  module, replacements = string.gsub(module, "/", ".")
+  if replacements > 0 then
+    module = string.gsub(module, "%.lua$", "")
+  end
+  package.loaded[module] = nil
+  require(module)
+  vim.notify(string.format("Reloaded '%s'.", module))
+end, { bar = true, nargs = 1 })
+
+-- https://stackoverflow.com/a/2573758
+-- Inspired by the TabMessage function/command combo found at <http://www.jukie.net/~bart/conf/vimrc>.
+vim.api.nvim_create_user_command("RedirMessages", function(command)
+  vim.cmd("redir => message")
+  vim.cmd(string.format("silent %s", command.args))
+  vim.cmd("redir END")
+  vim.cmd("silent put=message")
+end, { complete = "command", nargs = "+" })
+
 -- https://vi.stackexchange.com/a/36414
 local function source(line_start, line_end, vimscript)
   local tempfile = vim.fn.tempname()
@@ -164,24 +201,9 @@ end
 vim.api.nvim_create_user_command("Source", function(command)
   source(command.line1, command.line2)
 end, { nargs = 0, range = true })
-vim.api.nvim_create_user_command("VimscriptSource", function(command)
-  source(command.line1, command.line2, true)
-end, { nargs = 0, range = true })
 
-vim.api.nvim_create_user_command("Filter", function(command)
-  local line_start = command.line1
-  local line_end = command.line2
-  local offset = 0
-  for linenr = line_start, line_end do
-    vim.api.nvim_win_set_cursor(0, { linenr + offset, 0 })
-    local output = vim.fn.systemlist(vim.fn.getline(linenr + offset))
-    vim.cmd.delete()
-    vim.fn.append(linenr + offset - 1, output)
-    if not vim.tbl_isempty(output) then
-      offset = offset + #output - 1
-    end
-  end
-  vim.api.nvim_win_set_cursor(0, { line_start, 0 })
+vim.api.nvim_create_user_command("SourceVimscript", function(command)
+  source(command.line1, command.line2, true)
 end, { nargs = 0, range = true })
 
 -- Human-readable stack of syntax items
@@ -193,15 +215,6 @@ vim.api.nvim_create_user_command("Synstack", function()
   end, synstack)
   print(vim.inspect(syn_id_addrs))
 end, { nargs = 0 })
-
--- https://stackoverflow.com/a/2573758
--- Inspired by the TabMessage function/command combo found at <http://www.jukie.net/~bart/conf/vimrc>.
-vim.api.nvim_create_user_command("RedirMessages", function(command)
-  vim.cmd("redir => message")
-  vim.cmd(string.format("silent %s", command.args))
-  vim.cmd("redir END")
-  vim.cmd("silent put=message")
-end, { complete = "command", nargs = "+" })
 
 -- }}}
 
