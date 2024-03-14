792a5eab80efc0f09634953a80d63860305f35ae
lbrayner <brayner.l@gmail.com>
2024-03-14 14:09:19 -0300

    quickfix.lua: code review, buffer options

--- plugin/quickfix.lua
+++ plugin/quickfix.lua
@@ -33,39 +33,39 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "qf",
   callback = function(args)
     local bufnr = args.buf
+
+    vim.keymap.set("n", "o", function()
+      display_error_switchbuf("usetab,split")
+    end, { buffer = bufnr })
+    vim.keymap.set("n", "O", function()
+      display_error_switchbuf("usetab,vsplit")
+    end, { buffer = bufnr })
+    vim.keymap.set("n", "<Tab>", function()
+      display_error_switchbuf("usetab,newtab")
+    end, { buffer = bufnr })
+
+    vim.keymap.set("n", "<Leader>o", function()
+      display_error_cmd("split")
+    end, { buffer = bufnr })
+    vim.keymap.set("n", "<Leader>O", function()
+      display_error_cmd("vsplit")
+    end, { buffer = bufnr })
+    vim.keymap.set("n", "<Leader><Tab>", function()
+      display_error_cmd("tabnew")
+    end, { buffer = bufnr })
+
+    vim.keymap.set("n", "q", function()
+      vim.api.nvim_win_close(0, false)
+    end, { buffer = bufnr, nowait = true })
+
     local wininfos = vim.tbl_filter(function(wininfo)
       return wininfo.bufnr == bufnr
     end, vim.fn.getwininfo())
 
     for _, wininfo in ipairs(wininfos) do
       local winid = wininfo.winid
-
       vim.wo[winid].spell = false
       vim.wo[winid].wrap = false
-
-      vim.keymap.set("n", "o", function()
-        display_error_switchbuf("usetab,split")
-      end, { buffer = bufnr })
-      vim.keymap.set("n", "O", function()
-        display_error_switchbuf("usetab,vsplit")
-      end, { buffer = bufnr })
-      vim.keymap.set("n", "<Tab>", function()
-        display_error_switchbuf("usetab,newtab")
-      end, { buffer = bufnr })
-
-      vim.keymap.set("n", "<Leader>o", function()
-        display_error_cmd("split")
-      end, { buffer = bufnr })
-      vim.keymap.set("n", "<Leader>O", function()
-        display_error_cmd("vsplit")
-      end, { buffer = bufnr })
-      vim.keymap.set("n", "<Leader><Tab>", function()
-        display_error_cmd("tabnew")
-      end, { buffer = bufnr })
-
-      vim.keymap.set("n", "q", function()
-        vim.api.nvim_win_close(0, false)
-      end, { buffer = bufnr, nowait = true })
     end
 
     local function get_name_by_bufnr()
