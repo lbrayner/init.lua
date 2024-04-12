50865c1d2102f79f01acf2ec6ecf0a91a9b343e8
lbrayner <brayner.l@gmail.com>
2024-04-12 15:00:37 -0300

    No user_command, fixed Rt, extra line

--- lua/lbrayner/ripgrep.lua
+++ lua/lbrayner/ripgrep.lua
@@ -31,6 +31,7 @@ local function rg(txt, config_path, loclist)
     vim.bo.grepprg = ""
     return
   end
+
   vim.cmd(grep)
 end
 
@@ -42,68 +43,64 @@ function M.rg(txt, config_path)
   rg(txt, config_path)
 end
 
-function M.user_command(command, config_path)
-  local txt = command.args
-  local count = command.count
-  local line1 = command.line1
-  local line2 = command.line2
-
-  if count == 0 then -- :0Rg
-    local context = vim.fn.getqflist({ context = 1 }).context
-    if vim.tbl_get(context, "ripgrep", "txt") then
-      -- :0Rg performs a search with the last text juxtaposed with the new text
-      txt = vim.trim(table.concat({ context.ripgrep.txt, txt }, " "))
-    else
-      vim.notify("Could not find a ripgrep search context.")
-      return
-    end
-  elseif count > 0 then -- :'<,'>Rg
-    -- https://neovim.discourse.group/t/function-that-return-visually-selected-text/1601
-    local pos_start = vim.api.nvim_buf_get_mark(0, "<")
-    local pos_end = vim.api.nvim_buf_get_mark(0, ">")
-    if line1 ~= pos_start[1] or line2 ~= pos_end[1] then
-      vim.notify("Line range not allowed, only visual selection.")
-      return
-    end
-    if pos_start[1] ~= pos_end[1] then
-      vim.notify("Visual selection pattern cannot span multiple lines.")
-      return
-    end
-    local start_row = pos_start[1] - 1
-    local start_col = pos_start[2]
-    local end_row = pos_end[1] - 1
-    local end_col = pos_end[2] + 1
-    local visual_selection = vim.api.nvim_buf_get_text(0, start_row, start_col, end_row, end_col, {})[1]
-    txt = vim.trim(table.concat({
-      string.format("-s -F -e %s", vim.fn.shellescape(visual_selection)),
-      txt }, " "))
-  end
-
-  local success, err = pcall(M.rg, txt, config_path)
-
-  if not success then
-    vim.cmd.cclose()
-    print(vim.inspect(err)) -- TODO debug
-    if type(err) == "string" and require("lbrayner").contains(err, " Rg:") then
-      error(err)
-    end
-    vim.notify(string.format("Error searching for “%s”. Unmatched quotes? Check your command.", txt))
-    return
-  end
-
-  vim.fn.setqflist({}, "a", { context = { ripgrep = { txt = txt } } })
-
-  if not vim.tbl_isempty(vim.fn.getqflist()) then
-    vim.cmd("botright copen")
-  else
-    vim.cmd.cclose()
-    vim.notify(string.format("No match found for “%s”.", txt))
-  end
-end
-
 function M.with_config_path(command_name, config_path)
   vim.api.nvim_create_user_command(command_name, function(command)
-    M.user_command(command, config_path)
+    local txt = command.args
+    local count = command.count
+    local line1 = command.line1
+    local line2 = command.line2
+
+    if count == 0 then -- :0Rg
+      local context = vim.fn.getqflist({ context = 1 }).context
+      if vim.tbl_get(context, "ripgrep", "txt") then
+        -- :0Rg performs a search with the last text juxtaposed with the new text
+        txt = vim.trim(table.concat({ context.ripgrep.txt, txt }, " "))
+      else
+        vim.notify("Could not find a ripgrep search context.")
+        return
+      end
+    elseif count > 0 then -- :'<,'>Rg
+      -- https://neovim.discourse.group/t/function-that-return-visually-selected-text/1601
+      local pos_start = vim.api.nvim_buf_get_mark(0, "<")
+      local pos_end = vim.api.nvim_buf_get_mark(0, ">")
+      if line1 ~= pos_start[1] or line2 ~= pos_end[1] then
+        vim.notify("Line range not allowed, only visual selection.")
+        return
+      end
+      if pos_start[1] ~= pos_end[1] then
+        vim.notify("Visual selection pattern cannot span multiple lines.")
+        return
+      end
+      local start_row = pos_start[1] - 1
+      local start_col = pos_start[2]
+      local end_row = pos_end[1] - 1
+      local end_col = pos_end[2] + 1
+      local visual_selection = vim.api.nvim_buf_get_text(0, start_row, start_col, end_row, end_col, {})[1]
+      txt = vim.trim(table.concat({
+        string.format("-s -F -e %s", vim.fn.shellescape(visual_selection)),
+        txt }, " "))
+    end
+
+    local success, err = pcall(M.rg, txt, config_path)
+
+    if not success then
+      vim.cmd.cclose()
+      print(vim.inspect(err)) -- TODO debug
+      if type(err) == "string" and require("lbrayner").contains(err, " Rg:") then
+        error(err)
+      end
+      vim.notify(string.format("Error searching for “%s”. Unmatched quotes? Check your command.", txt))
+      return
+    end
+
+    vim.fn.setqflist({}, "a", { context = { ripgrep = { txt = txt } } })
+
+    if not vim.tbl_isempty(vim.fn.getqflist()) then
+      vim.cmd("botright copen")
+    else
+      vim.cmd.cclose()
+      vim.notify(string.format("No match found for “%s”.", txt))
+    end
   end, { complete = "file", nargs = "*", range = -1 })
 end
 
@@ -117,8 +114,7 @@ M.with_config_path("RgTests", ".ripgreprc-tests")
 
 vim.keymap.set("ca", "Rb", [[Rg -s -e'\b''\b'<Left><Left><Left><Left>]])
 vim.keymap.set("ca", "Rg", "Rg -e")
-vim.keymap.set("ca", "Rt", [[RgNoTests -s -e'\b''\b'<Left><Left><Left><Left>]])
+vim.keymap.set("ca", "Rt", [[RgTests -s -e'\b''\b'<Left><Left><Left><Left>]])
 vim.keymap.set("ca", "Rw", [[Rg -s -e'\b'<C-R><C-W>'\b']])
-
 
 return M
