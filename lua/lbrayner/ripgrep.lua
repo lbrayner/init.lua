7044a85ab68fe87274266fe7a197c6a5cada841c
lbrayner <brayner.l@gmail.com>
2024-04-12 11:13:36 -0300

    config_path

--- lua/lbrayner/ripgrep.lua
+++ lua/lbrayner/ripgrep.lua
@@ -2,46 +2,41 @@
 
 local M = {}
 
-local function rg(txt, loclist)
+local function rg(txt, config_path, loclist)
   local command = "grep!"
   if loclist then
     command = "lgrep!"
   end
   local rgopts = " "
-  if vim.go.ignorecase then
+  if vim.o.ignorecase then
     rgopts = rgopts.."-i "
   end
-  if vim.go.smartcase then
+  if vim.o.smartcase then
     rgopts = rgopts.."-S "
   end
-  if not vim.startswith(vim.go.grepprg, "rg") then
+  if not vim.startswith(vim.o.grepprg, "rg") and
+    not string.find(vim.o.grepprg, "^RIPGREP_CONFIG_PATH=.* rg") then
     error("Rg: 'grepprg' not correctly set.")
   end
   if not vim.fn.executable("rg") then
     error("Rg: 'rg' not executable.")
   end
+  if config_path then
+    vim.bo.grepprg = "RIPGREP_CONFIG_PATH=" .. config_path .. " " .. vim.go.grepprg
+  end
   -- Escaping Command-line special characters '#', '%' (:h :_%), and '|' (:h :bar)
   vim.cmd("silent "..command.." "..rgopts..vim.fn.escape(txt, "#%|"))
 end
 
-function M.lrg(txt)
-  rg(txt, true)
+function M.lrg(txt, config_path)
+  rg(txt, config_path, true)
 end
 
-function M.rg(txt)
-  rg(txt)
+function M.rg(txt, config_path)
+  rg(txt, config_path)
 end
 
-vim.go.grepprg = "rg --vimgrep --sort path"
-vim.go.grepformat = "%f:%l:%c:%m"
-vim.go.shellpipe = "&>"
-
-vim.api.nvim_create_user_command("Rg", function(command)
-  local txt = command.args
-  local count = command.count
-  local line1 = command.line1
-  local line2 = command.line2
-
+function M.user_command(txt, count, line1, line2, config_path)
   if count == 0 then -- :0Rg
     local context = vim.fn.getqflist({ context = 1 }).context
     if vim.tbl_get(context, "ripgrep", "txt") then
@@ -73,10 +68,11 @@ vim.api.nvim_create_user_command("Rg", function(command)
       txt }, " "))
   end
 
-  local success, err = pcall(M.rg, txt)
+  local success, err = pcall(M.rg, txt, config_path)
 
   if not success then
     vim.cmd.cclose()
+    print(vim.inspect(err)) -- TODO debug
     if type(err) == "string" and require("lbrayner").contains(err, " Rg:") then
       error(err)
     end
@@ -92,6 +88,33 @@ vim.api.nvim_create_user_command("Rg", function(command)
     vim.cmd.cclose()
     vim.notify(string.format("No match found for “%s”.", txt))
   end
+end
+
+-- local ripgrep = vim.api.nvim_create_augroup("ripgrep", { clear = true })
+--
+-- vim.api.nvim_create_autocmd("User", {
+--   pattern = "RipgrepConfigPath",
+--   group = ripgrep,
+--   callback = function()
+--     -- print("bo.grepprg", vim.bo.grepprg) -- TODO debug
+--     -- print("RIPGREP_CONFIG_PATH", vim.env.RIPGREP_CONFIG_PATH) -- TODO debug
+--     if vim.bo.grepprg == "" and vim.env.RIPGREP_CONFIG_PATH and vim.env.RIPGREP_CONFIG_PATH ~= "" then
+--       vim.bo.grepprg = "RIPGREP_CONFIG_PATH=" .. vim.env.RIPGREP_CONFIG_PATH .. " " .. vim.go.grepprg
+--     end
+--   end,
+-- })
+
+vim.go.grepprg = "rg --vimgrep --sort path"
+vim.go.grepformat = "%f:%l:%c:%m"
+vim.go.shellpipe = "&>"
+
+vim.api.nvim_create_user_command("Rg", function(command)
+  local txt = command.args
+  local count = command.count
+  local line1 = command.line1
+  local line2 = command.line2
+
+  M.user_command(txt, count, line1, line2)
 end, { complete = "file", nargs = "*", range = -1 })
 
 vim.keymap.set("ca", "Rg", "Rg -e")
