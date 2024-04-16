7e074a4b8ed0c4c95c0d7e66f7e31bed055bc32f
lbrayner <brayner.l@gmail.com>
2024-04-16 11:18:06 -0300

    lsp.lua: tsserver logDirectory
    CLI argument `--tsserver-log-file` was replaced with initialization
    option `tsserver.logDirectory`.

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -40,6 +40,14 @@ require("typescript").setup({
   server = {
     autostart = false,
     capabilities = capabilities,
+    init_options = {
+      logDirectory = (function()
+        local logdir = vim.fs.normalize("~/.local/share/typescript-language-server/log")
+        if vim.fn.isdirectory(logdir) == 1 then
+          return logdir
+        end
+      end)()
+    },
   },
 })
 
