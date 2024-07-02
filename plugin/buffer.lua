1d14c07277c8dea6ff29653407bbe104fbb43465
lbrayner <brayner.l@gmail.com>
2024-07-02 12:36:37 -0300

    buffer.lua: alphabetical autocmd event list, extra blank

--- plugin/buffer.lua
+++ plugin/buffer.lua
@@ -2,7 +2,7 @@
 -- https://github.com/neovim/neovim/issues/2127
 local buffer_optimization = vim.api.nvim_create_augroup("buffer_optimization", { clear = true })
 
-vim.api.nvim_create_autocmd({ "CursorHold", "BufWritePost", "BufRead", "BufLeave" }, {
+vim.api.nvim_create_autocmd({ "BufLeave", "BufRead", "BufWritePost", "CursorHold" }, {
   group = buffer_optimization,
   desc = "Setting swapfile flag to trigger SwapExists",
   callback = function(args)
@@ -39,6 +39,7 @@ vim.api.nvim_create_autocmd("VimEnter", {
         end
       end,
     })
+
     vim.api.nvim_create_autocmd("User", {
       pattern = "FugitiveChanged",
       group = checktime,
