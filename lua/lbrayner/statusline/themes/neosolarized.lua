3cf3b7e13fa45041967f840932e9ec7a1e2f9382
lbrayner <brayner.l@gmail.com>
2024-08-24 09:23:09 -0300

    User groups of insert and normal

--- lua/lbrayner/statusline/themes/neosolarized.lua
+++ lua/lbrayner/statusline/themes/neosolarized.lua
@@ -1,9 +1,23 @@
 local mapping = {
   insert = {
     StatusLine = { bg = "Added", fg = "Ignore" },
+    User1 = { bg = "Added", fg = "Todo" },
+    User2 = { bg = "Added", fg = "Ignore" },
+    User3 = { bg = "Added", fg = "Underlined" },
+    User4 = { bg = "Added", fg = "Special" },
+    User5 = { bg = "Added", fg = "Ignore" },
+    User6 = { bg = "Added", fg = "Comment" },
+    User9 = { bg = "Added", fg = "Underlined" },
   },
   normal = {
     StatusLine = { fg = "Identifier" },
+    User1 = { fg = "Todo" },
+    User2 = { fg = "Underlined" },
+    User3 = { fg = "Todo" },
+    User4 = { fg = "Comment" },
+    User5 = { fg = "Ignore" },
+    User6 = { fg = "Statement" },
+    User9 = { fg = "Special" },
   },
   search = {
     StatusLine = { bg = "Constant", fg = "Include" },
