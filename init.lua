f1fc340be04c9f468a217f9cfd89968aa8507082
lbrayner <brayner.l@gmail.com>
2024-09-22 10:27:43 -0300

    Installed rocks.nvim

--- init.lua
+++ init.lua
@@ -493,6 +493,41 @@ vim.api.nvim_create_autocmd("TabClosed", {
 
 -- }}}
 
+-- Subsection: rocks.nvim {{{
+
+local rocks_config = {
+  rocks_path = vim.fs.normalize("~/.local/share/nvim/rocks"),
+}
+
+vim.g.rocks_nvim = rocks_config
+
+local luarocks_path = {
+  vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?.lua"),
+  vim.fs.joinpath(rocks_config.rocks_path, "share", "lua", "5.1", "?", "init.lua"),
+}
+
+package.path = package.path .. ";" .. table.concat(luarocks_path, ";")
+
+local luarocks_cpath = {
+  vim.fs.joinpath(rocks_config.rocks_path, "lib", "lua", "5.1", "?.so"),
+  vim.fs.joinpath(rocks_config.rocks_path, "lib64", "lua", "5.1", "?.so"),
+}
+
+package.cpath = package.cpath .. ";" .. table.concat(luarocks_cpath, ";")
+
+local rocks_rtp = {
+  rocks_config.rocks_path,
+  "lib",
+  "luarocks",
+  "rocks-5.1",
+  "rocks.nvim",
+  "*"
+}
+
+vim.opt.runtimepath:append(vim.fs.joinpath(unpack(rocks_rtp)))
+
+-- }}}
+
 local vim_dir = vim.fn.stdpath("config")
 
 if not vim.env.MYVIMRC or vim.env.MYVIMRC == "" then
