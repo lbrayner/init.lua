c5eb84c01068ce07acf017cd38c33e20d1f98b77
lbrayner <brayner.l@gmail.com>
2024-09-25 12:29:56 -0300

    Moved jdtls code to config.lua and jdtls.lua

--- lua/lbrayner/config.lua
+++ lua/lbrayner/config.lua
@@ -59,6 +59,45 @@ if pcall(require, "dapui") then
   require("lbrayner.config.dap")
 end
 
+-- nvim-jdtls
+
+if pcall(require, "jdtls") then
+  local jdtls_start = vim.api.nvim_create_augroup("jdtls_start", { clear = true })
+
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "java",
+    group = jdtls_start,
+    callback = function(args)
+      local bufnr = args.buf
+
+      vim.api.nvim_buf_create_user_command(bufnr, "JdtStart", function(command)
+        local config
+        local success, session = pcall(require, "lbrayner.session.jdtls")
+
+        if success then
+          config = session.get_config()
+        else
+          config = require("lbrayner.jdtls").get_config()
+        end
+
+        local client = vim.lsp.get_clients({ name = "jdtls" })[1]
+
+        if not command.bang and client then
+          require("jdtls").start_or_attach(config)
+          return
+        end
+
+        require("lbrayner.jdtls").setup(config)
+      end,
+      {
+        bang = true,
+        desc = "Start jdtls and setup automatic attach, local buffer configuration",
+        nargs = 0
+      })
+    end,
+  })
+end
+
 -- nvim-lspconfig
 
 if pcall(require, "lspconfig") then
