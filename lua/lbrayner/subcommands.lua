8c93cff3e883bc5bb6839d86b96a5b9d053ac946
lbrayner <brayner.l@gmail.com>
2024-11-07 15:08:53 -0300

    No need to process, no need to schedule, can add subcommands

--- lua/lbrayner/subcommands.lua
+++ lua/lbrayner/subcommands.lua
@@ -7,16 +7,6 @@ function M.complete_filename(lead)
 end
 
 local function main_cmd(name, subcommand_tbl)
-  for subcommand_key, subcommand in pairs(subcommand_tbl) do
-    if subcommand.simple and type(subcommand.simple) == "function" then
-      local simple = subcommand.simple
-      subcommand.impl = function(args, opts)
-        assert(vim.tbl_isempty(args), string.format("Trailing characters: %s", table.concat(args, " ")))
-        simple(opts)
-      end
-      subcommand.simple = nil
-    end
-  end
   ---@param opts table :h lua-guide-commands-create
   return function(opts)
     local fargs = opts.fargs
@@ -29,7 +19,12 @@ local function main_cmd(name, subcommand_tbl)
       return
     end
     -- Invoke the subcommand
-    subcommand.impl(args, opts)
+    if subcommand.simple and type(subcommand.simple) == "function" then
+      assert(vim.tbl_isempty(args), string.format("Trailing characters: %s", table.concat(args, " ")))
+      simple(opts)
+    else
+      subcommand.impl(args, opts)
+    end
   end
 end
 
@@ -38,35 +33,33 @@ function M.create_command_and_subcommands(name, subcommand_tbl, opts)
   assert(name:match("^%u%a+$"), "Bad argument; 'name' must a capitalized word.")
   assert(type(subcommand_tbl) == "table", "'subcommand_tbl' must be a table")
   assert(type(opts) == "table", "'opts' must be a table")
-  vim.schedule(function()
-    vim.api.nvim_create_user_command(name, main_cmd(name, subcommand_tbl), {
-      nargs = "+",
-      desc = opts.desc,
-      complete = function(arg_lead, cmdline, _)
-        -- Get the subcommand.
-        local subcmd_key, subcmd_arg_lead = cmdline:match("^['<,'>]*" .. name .. "[!]*%s(%S+)%s(.*)$")
-        if subcmd_key
-          and subcmd_arg_lead
-          and subcommand_tbl[subcmd_key]
-          and subcommand_tbl[subcmd_key].complete then
-          -- The subcommand has completions. Return them.
-          return subcommand_tbl[subcmd_key].complete(subcmd_arg_lead)
-        end
-        -- Check if cmdline is a subcommand
-        if cmdline:match("^['<,'>]*" .. name .. "[!]*%s+%w*$") then
-          -- Filter subcommands that match
-          local subcommand_keys = vim.tbl_keys(subcommand_tbl)
-          return vim.iter(subcommand_keys)
-          :filter(function(key)
-            return key:find(arg_lead) ~= nil
-          end)
-          :totable()
-        end
-      end,
-      bang = opts.bang,
-      range = opts.range
-    })
-  end)
+  vim.api.nvim_create_user_command(name, main_cmd(name, subcommand_tbl), {
+    nargs = "+",
+    desc = opts.desc,
+    complete = function(arg_lead, cmdline, _)
+      -- Get the subcommand.
+      local subcmd_key, subcmd_arg_lead = cmdline:match("^['<,'>]*" .. name .. "[!]*%s(%S+)%s(.*)$")
+      if subcmd_key
+        and subcmd_arg_lead
+        and subcommand_tbl[subcmd_key]
+        and subcommand_tbl[subcmd_key].complete then
+        -- The subcommand has completions. Return them.
+        return subcommand_tbl[subcmd_key].complete(subcmd_arg_lead)
+      end
+      -- Check if cmdline is a subcommand
+      if cmdline:match("^['<,'>]*" .. name .. "[!]*%s+%w*$") then
+        -- Filter subcommands that match
+        local subcommand_keys = vim.tbl_keys(subcommand_tbl)
+        return vim.iter(subcommand_keys)
+        :filter(function(key)
+          return key:find(arg_lead) ~= nil
+        end)
+        :totable()
+      end
+    end,
+    bang = opts.bang,
+    range = opts.range
+  })
 end
 
 return M
