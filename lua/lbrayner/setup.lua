627c6b326986915307c23bfe3528ad928f0374ea
lbrayner <brayner.l@gmail.com>
2025-01-24 10:21:06 -0300

    Moved lbrayner.setup.dap code to lbrayner.setup

--- lua/lbrayner/setup.lua
+++ lua/lbrayner/setup.lua
@@ -56,7 +56,17 @@ end
 -- nvim-dap-ui
 
 if pcall(require, "dapui") then
-  require("lbrayner.setup.dap")
+  require("dapui").setup()
+
+  vim.api.nvim_create_user_command("DapUiClose", function(command)
+    require("dapui").close({ layout = tonumber(command.args) })
+  end, { nargs = "?" })
+
+  vim.api.nvim_create_user_command("DapUiOpen", function(command)
+    require("dapui").open({ layout = tonumber(command.args) })
+  end, { nargs = "?" })
+
+  vim.api.nvim_create_user_command("DapUiReset", require("dapui").setup, { nargs = 0 })
 end
 
 -- nvim-lspconfig
