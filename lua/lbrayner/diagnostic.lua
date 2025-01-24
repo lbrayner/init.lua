184bb6686f751a1e3af0e9de9123c3d3a423f37c
lbrayner <brayner.l@gmail.com>
2025-01-24 09:16:02 -0300

    Diagnostic: reviewed code, things changed
    No need to highlight.

--- lua/lbrayner/diagnostic.lua
+++ lua/lbrayner/diagnostic.lua
@@ -1,16 +1,25 @@
+-- vim: fdm=marker
+
+-- {{{
+
 local function is_long(bufnr, winid, virt_texts, lnum)
   -- TODO reduce?
   local mess_len = 0
+
   for _, virt_text in ipairs(virt_texts) do
     mess_len = mess_len + string.len(virt_text[1])
   end
+
   if mess_len == 0 then
     return false
   end
+
   -- Dealing with E5555: API call: Index out of bounds
   local success, lines = pcall(vim.api.nvim_buf_get_lines, bufnr, lnum, lnum+1, true)
+
   if not success then return false end
-  local line = lines[1]
+
+  local _, line = next(lines)
   local line_len = string.len(line)
   local winwidth = vim.api.nvim_win_get_width(winid) - 2 - 3 -- sign & column number
   local long = line_len + 1 + mess_len > winwidth
@@ -19,27 +28,31 @@ end
 
 local function handle_long_extmarks(namespace, bufnr, winid)
   local metadata = vim.diagnostic.get_namespace(namespace)
-  if not metadata then
-    return
-  end
+
+  if not metadata then return end
+
   local virt_text_ns = metadata.user_data.virt_text_ns
-  if not virt_text_ns then
-    return
-  end
+
+  if not virt_text_ns then return end
+
   local extmarks = vim.api.nvim_buf_get_extmarks(bufnr, virt_text_ns, 0, -1, {
     details = true
   })
+
   for _, extmark in ipairs(extmarks) do
     local id, lnum, details = extmark[1], extmark[2], extmark[4]
-    if not details.virt_text then
-      return
-    end
+
+    if not details.virt_text then return end
+
     local long = is_long(bufnr, winid, details.virt_text, lnum)
+
     if long then
       vim.api.nvim_buf_del_extmark(bufnr, virt_text_ns, id)
     end
   end
 end
+
+-- }}}
 
 local trunc_virt_text = vim.api.nvim_create_augroup("trunc_virt_text", { clear = true })
 
@@ -51,6 +64,7 @@ vim.api.nvim_create_autocmd("VimEnter", {
       callback = function(args)
         local bufnr = args.buf
         local winid = vim.api.nvim_get_current_win()
+
         for _, namespace in ipairs(vim.tbl_values(vim.api.nvim_get_namespaces())) do
           handle_long_extmarks(namespace, bufnr, winid)
         end
@@ -61,12 +75,15 @@ vim.api.nvim_create_autocmd("VimEnter", {
       group = trunc_virt_text,
       callback = function(args)
         local winids = vim.v.event.windows
+
         local wininfos = vim.tbl_filter(function(wininfo)
           return vim.tbl_contains(winids, wininfo.winid)
         end, vim.fn.getwininfo())
+
         for _, wininfo in ipairs(wininfos) do
           local bufnr = wininfo.bufnr
           local winid = wininfo.winid
+
           for _, namespace in ipairs(vim.tbl_values(vim.api.nvim_get_namespaces())) do
             handle_long_extmarks(namespace, bufnr, winid)
           end
@@ -121,8 +138,6 @@ vim.keymap.set("n", "]!", function()
   } })
 end, opts)
 
-local custom_diagnostics = vim.api.nvim_create_augroup("custom_diagnostics", { clear = true })
-
 local err = "DiagnosticSignError"
 local war = "DiagnosticSignWarn"
 local inf = "DiagnosticSignInfo"
@@ -137,12 +152,7 @@ if not _G.default_virtual_text_handler then
   _G.default_virtual_text_handler = vim.diagnostic.handlers.virtual_text
 end
 
-local function DefaultDiagnostics()
-  vim.cmd "highlight DiagnosticError guifg=Red"
-  vim.cmd "highlight DiagnosticWarn  guifg=Orange"
-  vim.cmd "highlight DiagnosticInfo  guifg=LightBlue"
-  vim.cmd "highlight DiagnosticHint  guifg=LightGrey"
-
+vim.api.nvim_create_user_command("DefaultDiagnostics", function()
   vim.diagnostic.config({
     signs = {
       text = {
@@ -175,11 +185,9 @@ local function DefaultDiagnostics()
   })
 
   vim.diagnostic.handlers.virtual_text = _G.default_virtual_text_handler
-end
+end, { nargs = 0 })
 
-vim.api.nvim_create_user_command("DefaultDiagnostics", DefaultDiagnostics, { nargs = 0 })
-
-local function CustomDiagnostics()
+local function customize_diagnostics()
   vim.diagnostic.config({
     signs = {
       text = {
@@ -217,12 +225,13 @@ local function CustomDiagnostics()
   vim.diagnostic.handlers.virtual_text = {
     show = function(namespace, bufnr, diagnostics, opts)
       _G.default_virtual_text_handler.show(namespace, bufnr, diagnostics, opts)
+
       local wininfos = vim.tbl_filter(function(wininfo)
         return wininfo.bufnr == bufnr
       end, vim.fn.getwininfo())
+
       for _, wininfo in ipairs(wininfos) do
-        local winid = wininfo.winid
-        handle_long_extmarks(namespace, bufnr, winid)
+        handle_long_extmarks(namespace, bufnr, wininfo.winid)
       end
     end,
     hide = function(namespace, bufnr)
@@ -231,9 +240,11 @@ local function CustomDiagnostics()
   }
 end
 
-vim.api.nvim_create_user_command("CustomDiagnostics", CustomDiagnostics, { nargs = 0 })
+vim.api.nvim_create_user_command("CustomDiagnostics", customize_diagnostics, { nargs = 0 })
+
+local custom_diagnostics = vim.api.nvim_create_augroup("custom_diagnostics", { clear = true })
 
 vim.api.nvim_create_autocmd("VimEnter", {
   group = custom_diagnostics,
-  callback = CustomDiagnostics,
+  callback = customize_diagnostics,
 })
