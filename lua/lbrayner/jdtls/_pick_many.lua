8c831529faf92d35c603a8ebbefacf9f7765bf42
lbrayner <brayner.l@gmail.com>
2025-05-03 18:28:26 -0300

    Won't work, async

--- lua/lbrayner/jdtls/_pick_many.lua
+++ lua/lbrayner/jdtls/_pick_many.lua
@@ -1,4 +1,4 @@
 require("jdtls.ui").pick_many = function(items)
   print("slim shady", vim.inspect(items))
-  return items[1]
+  return require("fzf-lua").fzf_exec(items, { prompt = "Projects> " })
 end
