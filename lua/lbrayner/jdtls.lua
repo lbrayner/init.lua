d1615972e20d619b5775c7e03edab01b3cb15027
lbrayner <brayner.l@gmail.com>
2025-05-06 16:09:37 -0300

    jdtls: reloadable modules _dap and _operations

--- lua/lbrayner/jdtls.lua
+++ lua/lbrayner/jdtls.lua
@@ -59,180 +59,24 @@ function M.get_config()
   }
 end
 
-local offset_encoding = "utf-16"
-local SymbolKind = vim.lsp.protocol.SymbolKind
-
-function M.java_go_to_top_level_declaration()
-  local bufnr = vim.api.nvim_get_current_buf()
-
-  local clients = vim.lsp.get_clients({ bufnr = bufnr, name = "jdtls" })
-  local _, client = next(clients)
-
-  -- From nvim-jdtls
-  if not client then
-    vim.notify("No LSP client with name `jdtls` available", vim.log.levels.WARN)
-    return
-  end
-
-  local params = { textDocument = vim.lsp.util.make_text_document_params(bufnr) }
-
-  client:request("textDocument/documentSymbol", params, function(err, result, ctx)
-    assert(not err, vim.inspect(err))
-
-    if vim.tbl_isempty(result) then
-      vim.notify("Go to top level declaration: no document symbols found", vim.log.levels.ERROR)
-      return
-    end
-
-    local top_level_symbols = vim.tbl_filter(function(symbol)
-      return vim.tbl_contains({
-        SymbolKind.Class,
-        SymbolKind.Enum,
-        SymbolKind.Interface
-      }, symbol.kind)
-    end, result)
-
-    if vim.tbl_count(top_level_symbols) > 1 then
-      -- Removing children
-      top_level_symbols = vim.tbl_map(function(symbol)
-        symbol.children = nil
-        return symbol
-      end, top_level_symbols)
-
-      local title = string.format("Top level symbols in %s",
-        vim.fn.fnamemodify(vim.api.nvim_buf_get_name(bufnr), ":."))
-      local items = vim.lsp.util.symbols_to_items(top_level_symbols, bufnr)
-
-      vim.fn.setqflist({}, " ", { title = title, items = items, context = ctx })
-      vim.api.nvim_command("botright copen")
-      return
-    end
-
-    vim.lsp.util.show_document({
-      uri = params.textDocument.uri, range = top_level_symbols[1].selectionRange
-    }, offset_encoding)
-  end, bufnr)
+do
+  local operations_mt = {
+    __index = function(_, key)
+      return require("lbrayner.jdtls._operations")[key]
+    end,
+    __newindex = function()
+      error("Cannot add item")
+    end,
+  }
+  M.operations = setmetatable({}, operations_mt)
 end
 
-local maximum_resolve_depth = 10
+function M.java_go_to_top_level_declaration()
+  M.operations.java_go_to_top_level_declaration()
+end
 
--- Type hierarchy on quickfix list
 function M.java_type_hierarchy(opts)
-  local function resolve_command(result)
-    return {
-      command = "java.navigate.resolveTypeHierarchy",
-      arguments = {
-        vim.fn.json_encode(result), -- toResolve: TypeHierarchyItem
-        "1", -- direction: Children(0), Parents(1), Both(2)
-        "1", -- resolveDepth
-      },
-    }
-  end
-
-  local hierarchy = {}
-  local depth = 0
-  local open_type_hierarchy
-
-  local function resolve_handler(err, result, ctx)
-    assert(not err, vim.inspect(err))
-    depth = depth + 1
-
-    local parents = result.parents
-
-    if not vim.tbl_isempty(parents) and depth > maximum_resolve_depth then
-      vim.notify(string.format("Type hierarchy: maximum resolve depth is %d.", maximum_resolve_depth),
-        vim.log.levels.WARN)
-    elseif not vim.tbl_isempty(parents) then
-      local parent_classes = vim.tbl_filter(function(parent)
-        return parent.kind == SymbolKind.Class
-      end, parents)
-
-      assert(vim.tbl_count(parent_classes) <= 1, "Type hierarchy: more than one parent class")
-
-      local parent = parent_classes[1]
-      if not parent then
-        assert(vim.tbl_count(parents) == 1,
-          string.format("Type hierarchy: could not determine parent with result %s",
-          vim.inspect(result)))
-        -- Symbol at point is a SymbolKind.Method
-        parent = parents[1]
-      end
-
-      vim.list_extend(hierarchy, parents)
-
-      require("jdtls.util").execute_command(resolve_command(parent), resolve_handler)
-      return
-    end
-
-    if not vim.tbl_isempty(hierarchy) then
-      local root = hierarchy[#hierarchy]
-      if root.detail.."."..root.name == "java.lang.Object" then
-        table.remove(hierarchy) -- Pop the top
-      end
-    end
-
-    if vim.tbl_isempty(hierarchy) then
-      vim.notify("Type hierarchy: no results.")
-      return
-    end
-
-    local locations = vim.tbl_map(function(parent)
-      return { uri = parent.uri, range = parent.selectionRange }
-    end, hierarchy)
-
-    local title = string.format("Type hierarchy: %s.%s", open_type_hierarchy.detail, open_type_hierarchy.name)
-    if vim.tbl_get(open_type_hierarchy, "data", "method_name") then
-      title = string.format("%s.%s", title, open_type_hierarchy.data.method_name)
-    end
-
-    hierarchy = nil
-    depth = nil
-    open_type_hierarchy = nil
-
-    local items = {}
-    for _, location in ipairs(locations) do -- Preserving table order
-      table.insert(items, vim.lsp.util.locations_to_items({ location }, offset_encoding)[1])
-    end
-
-    if opts.on_list then
-      assert(type(opts.on_list) == "function", "on_list is not a function")
-      opts.on_list({ title = title, items = items, context = ctx })
-      return
-    end
-
-    if vim.tbl_count(locations) == 1  then
-      vim.lsp.util.show_document(locations[1], offset_encoding, opts.reuse_win)
-      return
-    end
-
-    vim.fn.setqflist({}, " ", { title = title, items = items, context = ctx })
-    vim.api.nvim_command("botright copen")
-  end
-
-  opts = opts or {
-    on_list = require("lbrayner.lsp").on_list,
-    reuse_win = true
-  }
-
-  local position = vim.lsp.util.make_position_params(0, offset_encoding)
-  local command = {
-    command = "java.navigate.openTypeHierarchy",
-    arguments = {
-      vim.fn.json_encode(position), -- textParams: TextDocumentPositionParams
-      "1", -- direction: Children(0), Parents(1), Both(2)
-      "0", -- resolveDepth
-    },
-  }
-
-  require("jdtls.util").execute_command(command, function(err, result)
-    assert(not err, vim.inspect(err))
-    if not result then
-      vim.notify("Type hierarchy: openTypeHierarchy returned no results", vim.log.levels.ERROR)
-      return
-    end
-    open_type_hierarchy = result
-    require("jdtls.util").execute_command(resolve_command(result), resolve_handler)
-  end)
+  M.operations.java_type_hierarchy(opts)
 end
 
 function M.setup(config)
@@ -312,51 +156,20 @@ function M.setup(config)
   require("jdtls").start_or_attach(config)
 end
 
--- Modified test_class from nvim-jdtls/lua/jdtls/dap.lua so that additional
--- vmArgs can be supplied via environment variable
+do
+  local dap_mt = {
+    __index = function(_, key)
+      return require("lbrayner.jdtls._dap")[key]
+    end,
+    __newindex = function()
+      error("Cannot add item")
+    end,
+  }
+  M.dap = setmetatable({}, dap_mt)
+end
 
---- @param opts JdtTestOpts|nil
 function M.test_class(opts)
-  -- nvim-jdtls internal function
-  local function get_first_class_lens(lenses)
-    for _, lens in pairs(lenses) do
-      -- compatibility for versions prior to
-      -- https://github.com/microsoft/vscode-java-test/pull/1257
-      -- LegacyTestLevel.Class is 3
-      if lens.level == 3 then
-        return lens
-      end
-      -- TestLevel.Class is 5
-      if lens.testLevel == 5 then
-        return lens
-      end
-    end
-  end
-
-  opts = opts or {}
-  local context = require("jdtls.dap").experimental.make_context(opts.bufnr)
-  require("jdtls.dap").experimental.fetch_lenses(context, function(lenses)
-    local lens = get_first_class_lens(lenses)
-    if not lens then
-      vim.notify("No test class found")
-      return
-    end
-    require("jdtls.dap").experimental.fetch_launch_args(lens, context, function(launch_args)
-      -- JdtTestOpts.config_overrides of type JdtDapConfig completely overrides
-      -- arguments, such as vmArgs. See function `make_config` in
-      -- nvim-jdtls/lua/jdtls/dap.lua, which is invoked in the following
-      -- statement.
-      local config = require("jdtls.dap").experimental.make_config(lens, launch_args, opts.config_overrides)
-      -- Get extra JVM args from environment
-      local dap_jvm_args = os.getenv("DAP_JVM_ARGS")
-
-      if dap_jvm_args and dap_jvm_args ~= "" then
-        config.vmArgs = config.vmArgs .. " " .. dap_jvm_args
-      end
-
-      require("jdtls.dap").experimental.run(lens, config, context, opts)
-    end)
-  end)
+  M.dap.test_class(opts)
 end
 
 return M
---
+++ lua/lbrayner/jdtls/_operations.lua
@@ -0,0 +1,179 @@
+local M = {}
+
+local offset_encoding = "utf-16"
+local SymbolKind = vim.lsp.protocol.SymbolKind
+
+local maximum_resolve_depth = 10
+
+function M.java_go_to_top_level_declaration()
+  local bufnr = vim.api.nvim_get_current_buf()
+
+  local clients = vim.lsp.get_clients({ bufnr = bufnr, name = "jdtls" })
+  local _, client = next(clients)
+
+  -- From nvim-jdtls
+  if not client then
+    vim.notify("No LSP client with name `jdtls` available", vim.log.levels.WARN)
+    return
+  end
+
+  local params = { textDocument = vim.lsp.util.make_text_document_params(bufnr) }
+
+  client:request("textDocument/documentSymbol", params, function(err, result, ctx)
+    assert(not err, vim.inspect(err))
+
+    if vim.tbl_isempty(result) then
+      vim.notify("Go to top level declaration: no document symbols found", vim.log.levels.ERROR)
+      return
+    end
+
+    local top_level_symbols = vim.tbl_filter(function(symbol)
+      return vim.tbl_contains({
+        SymbolKind.Class,
+        SymbolKind.Enum,
+        SymbolKind.Interface
+      }, symbol.kind)
+    end, result)
+
+    if vim.tbl_count(top_level_symbols) > 1 then
+      -- Removing children
+      top_level_symbols = vim.tbl_map(function(symbol)
+        symbol.children = nil
+        return symbol
+      end, top_level_symbols)
+
+      local title = string.format("Top level symbols in %s",
+        vim.fn.fnamemodify(vim.api.nvim_buf_get_name(bufnr), ":."))
+      local items = vim.lsp.util.symbols_to_items(top_level_symbols, bufnr)
+
+      vim.fn.setqflist({}, " ", { title = title, items = items, context = ctx })
+      vim.api.nvim_command("botright copen")
+      return
+    end
+
+    vim.lsp.util.show_document({
+      uri = params.textDocument.uri, range = top_level_symbols[1].selectionRange
+    }, offset_encoding)
+  end, bufnr)
+end
+
+-- Type hierarchy on quickfix list
+function M.java_type_hierarchy(opts)
+  local function resolve_command(result)
+    return {
+      command = "java.navigate.resolveTypeHierarchy",
+      arguments = {
+        vim.fn.json_encode(result), -- toResolve: TypeHierarchyItem
+        "1", -- direction: Children(0), Parents(1), Both(2)
+        "1", -- resolveDepth
+      },
+    }
+  end
+
+  local hierarchy = {}
+  local depth = 0
+  local open_type_hierarchy
+
+  local function resolve_handler(err, result, ctx)
+    assert(not err, vim.inspect(err))
+    depth = depth + 1
+
+    local parents = result.parents
+
+    if not vim.tbl_isempty(parents) and depth > maximum_resolve_depth then
+      vim.notify(string.format("Type hierarchy: maximum resolve depth is %d.", maximum_resolve_depth),
+        vim.log.levels.WARN)
+    elseif not vim.tbl_isempty(parents) then
+      local parent_classes = vim.tbl_filter(function(parent)
+        return parent.kind == SymbolKind.Class
+      end, parents)
+
+      assert(vim.tbl_count(parent_classes) <= 1, "Type hierarchy: more than one parent class")
+
+      local parent = parent_classes[1]
+      if not parent then
+        assert(vim.tbl_count(parents) == 1,
+          string.format("Type hierarchy: could not determine parent with result %s",
+          vim.inspect(result)))
+        -- Symbol at point is a SymbolKind.Method
+        parent = parents[1]
+      end
+
+      vim.list_extend(hierarchy, parents)
+
+      require("jdtls.util").execute_command(resolve_command(parent), resolve_handler)
+      return
+    end
+
+    if not vim.tbl_isempty(hierarchy) then
+      local root = hierarchy[#hierarchy]
+      if root.detail.."."..root.name == "java.lang.Object" then
+        table.remove(hierarchy) -- Pop the top
+      end
+    end
+
+    if vim.tbl_isempty(hierarchy) then
+      vim.notify("Type hierarchy: no results.")
+      return
+    end
+
+    local locations = vim.tbl_map(function(parent)
+      return { uri = parent.uri, range = parent.selectionRange }
+    end, hierarchy)
+
+    local title = string.format("Type hierarchy: %s.%s", open_type_hierarchy.detail, open_type_hierarchy.name)
+    if vim.tbl_get(open_type_hierarchy, "data", "method_name") then
+      title = string.format("%s.%s", title, open_type_hierarchy.data.method_name)
+    end
+
+    hierarchy = nil
+    depth = nil
+    open_type_hierarchy = nil
+
+    local items = {}
+    for _, location in ipairs(locations) do -- Preserving table order
+      table.insert(items, vim.lsp.util.locations_to_items({ location }, offset_encoding)[1])
+    end
+
+    if opts.on_list then
+      assert(type(opts.on_list) == "function", "on_list is not a function")
+      opts.on_list({ title = title, items = items, context = ctx })
+      return
+    end
+
+    if vim.tbl_count(locations) == 1  then
+      vim.lsp.util.show_document(locations[1], offset_encoding, opts.reuse_win)
+      return
+    end
+
+    vim.fn.setqflist({}, " ", { title = title, items = items, context = ctx })
+    vim.api.nvim_command("botright copen")
+  end
+
+  opts = opts or {
+    on_list = require("lbrayner.lsp").on_list,
+    reuse_win = true
+  }
+
+  local position = vim.lsp.util.make_position_params(0, offset_encoding)
+  local command = {
+    command = "java.navigate.openTypeHierarchy",
+    arguments = {
+      vim.fn.json_encode(position), -- textParams: TextDocumentPositionParams
+      "1", -- direction: Children(0), Parents(1), Both(2)
+      "0", -- resolveDepth
+    },
+  }
+
+  require("jdtls.util").execute_command(command, function(err, result)
+    assert(not err, vim.inspect(err))
+    if not result then
+      vim.notify("Type hierarchy: openTypeHierarchy returned no results", vim.log.levels.ERROR)
+      return
+    end
+    open_type_hierarchy = result
+    require("jdtls.util").execute_command(resolve_command(result), resolve_handler)
+  end)
+end
+
+return M
