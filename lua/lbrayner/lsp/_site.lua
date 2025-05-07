f859e61d4415da467227661e6b6113f5f9c4f539
lbrayner <brayner.l@gmail.com>
2025-05-07 12:42:57 -0300

    LSP: generic site local function discovery heuristics

--- lua/lbrayner/lsp/_site.lua
+++ lua/lbrayner/lsp/_site.lua
@@ -1,11 +1,23 @@
-local M = {}
+local success, site = pcall(require, "lbrayner.site.lsp")
 
-function M.is_test_file(filename)
-  local success, site = pcall(require, "lbrayner.site.lsp")
-
-  if success then
-    return site.is_test_file(filename)
-  end
+if not success then
+  site = nil
 end
 
-return M
+return setmetatable({}, {
+  __index = function(_, key)
+    if vim.list_contains(
+      {
+        "is_test_file",
+      }, key) then
+      if site and site[key] and type(site[key]) == "function" then
+        return site[key]
+      else
+        return function() end -- noop
+      end
+    end
+  end,
+  __newindex = function()
+    error("Cannot add item")
+  end,
+})
