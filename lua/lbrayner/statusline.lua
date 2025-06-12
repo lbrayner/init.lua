37d1fbf9d0bd3f178a4d52a6928e0d5c41464b8f
lbrayner <brayner.l@gmail.com>
2025-06-12 17:37:30 -0300

    statusline: redraw bug was fixed on 'nightly'

--- lua/lbrayner/statusline.lua
+++ lua/lbrayner/statusline.lua
@@ -347,13 +347,6 @@ vim.api.nvim_create_autocmd("CmdlineEnter", {
   callback = function(args)
     local cmdline_char = args.file
 
-    -- "@": do not redraw if waiting for input after input()
-    -- "-": do not redraw if waiting for input after z= or inputlist()
-    -- See https://github.com/neovim/neovim/issues/32068
-    if vim.tbl_contains({ "@", "-" }, cmdline_char) then
-      return
-    end
-
     if vim.tbl_contains({ "/", "?" }, cmdline_char) then
       M.highlight_mode("search")
     else
