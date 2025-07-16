0eb2b6a1894e9a0273e095726c66d92768740a50
lbrayner <brayner.l@gmail.com>
2025-07-16 12:00:53 -0300

    dapui: added a layout with just the console (for running programs)

--- lua/lbrayner/setup/dapui.lua
+++ lua/lbrayner/setup/dapui.lua
@@ -1,4 +1,37 @@
-require("dapui").setup()
+require("dapui").setup({
+  layouts = {
+    {
+      -- You can change the order of elements in the sidebar
+      elements = {
+        -- Provide IDs as strings or tables with "id" and "size" keys
+        {
+          id = "scopes",
+          size = 0.25, -- Can be float or integer > 1
+        },
+        { id = "breakpoints", size = 0.25 },
+        { id = "stacks", size = 0.25 },
+        { id = "watches", size = 0.25 },
+      },
+      size = 40,
+      position = "left", -- Can be "left" or "right"
+    },
+    {
+      elements = {
+        "repl",
+        "console",
+      },
+      size = 10,
+      position = "bottom", -- Can be "bottom" or "top"
+    },
+    {
+      elements = {
+        "console",
+      },
+      size = 10,
+      position = "bottom", -- Can be "bottom" or "top"
+    },
+  },
+})
 
 vim.api.nvim_create_user_command("DapUiClose", function(opts)
   require("dapui").close({ layout = tonumber(opts.args) })
