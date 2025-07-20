e898d238766a5870b1016e6e1f566fad40beb007
lbrayner <brayner.l@gmail.com>
2025-07-19 23:20:40 -0300

    Feature: async ripgrep

--- lua/lbrayner/diff.lua
+++ lua/lbrayner/diff.lua
@@ -1,14 +1,15 @@
 vim.keymap.set("n", "<Leader>di", "<Cmd>windo diffthis<CR>")
+vim.keymap.set("n", "<Leader>do", "<Cmd>diffoff!<CR>")
 vim.keymap.set("n", "<Leader>dw", function() -- toggle iwhite
   if require("lbrayner").contains(vim.go.diffopt, "iwhite") then
     vim.opt.diffopt:remove({ "iwhite" })
     vim.notify("-iwhite")
     return
   end
+
   vim.opt.diffopt:append({ "iwhite" })
   vim.notify("+iwhite")
 end)
-vim.keymap.set("n", "<Leader>do", "<Cmd>diffoff!<CR>")
 
 -- TODO for tackling a vim-fugitive bug, reproduce and submit a bug report
 local diffupdate = vim.api.nvim_create_augroup("diffupdate", { clear = false })
@@ -17,64 +18,95 @@ vim.api.nvim_create_autocmd("TabEnter" , {
   group = diffupdate,
   callback = function(args)
     local winid = vim.fn.bufwinid(args.buf)
+
     if not winid then return end
+
     if vim.wo[winid].diff then
       vim.cmd.diffupdate()
     end
   end,
 })
 
-local function update_conflict_markers(bufnr)
-  require("lbrayner.ripgrep").lrg([["^(<<<<<<<|\|\|\|\|\|\|\||=======|>>>>>>>)" ]] ..
-  vim.fn.shellescape(vim.api.nvim_buf_get_name(0)))
-  if not vim.tbl_isempty(vim.fn.getloclist(0)) then
-    vim.fn.setloclist(0, {}, "a", { title = "Conflict markers" })
-    return true
-  end
-  return false
-end
-
-local function clear_conflict_markers_autocmd(bufnr)
-  pcall(vim.api.nvim_del_autocmd, vim.b[bufnr].conflict_marker_autocmd)
-end
-
 vim.api.nvim_create_user_command("ConflictMarkers", function()
-  local function update_context(changedtick)
-    vim.fn.setloclist(0, {}, "a", { context = { conflict_markers = { changedtick = changedtick } } })
+  local bufnr, conflict_marker_autocmd, id = vim.api.nvim_get_current_buf()
+  local title = "Conflict markers"
+
+  local function clear_conflict_markers_autocmd()
+    pcall(vim.api.nvim_del_autocmd, conflict_marker_autocmd)
   end
 
-  local bufnr = vim.api.nvim_get_current_buf()
-  if update_conflict_markers(bufnr) then
-    clear_conflict_markers_autocmd(bufnr)
+  local function update_conflict_markers()
+    require("lbrayner.ripgrep").rg(
+      [["^(<<<<<<<|\|\|\|\|\|\|\||=======|>>>>>>>)" ]] ..
+      vim.fn.shellescape(vim.api.nvim_buf_get_name(0)), {
+        loclist = 0,
+        on_exit = function(obj, args, qfid)
+          local function cleanup()
+            clear_conflict_markers_autocmd()
 
-    local conflict_markers = vim.api.nvim_create_augroup("conflict_markers", { clear = false })
+            if id then
+              qflist = vim.fn.getloclist(0, { id = 0 })
 
-    vim.b[bufnr].conflict_marker_autocmd = vim.api.nvim_create_autocmd({ "BufWritePost", "WinEnter" }, {
-      group = conflict_markers,
-      buffer = bufnr,
-      callback = function(args)
-        local bufnr = args.buf
-        if vim.api.nvim_get_current_buf() ~= bufnr then
-          -- After a BufWritePost do nothing if bufnr is not current
-          return
-        end
-        if vim.fn.getloclist(0, { title = 1 }).title == "Conflict markers" then
-          local context = vim.fn.getloclist(0, { context = 1 }).context
-          if context.conflict_markers.changedtick < vim.b.changedtick then
-            if not update_conflict_markers(bufnr) then
-              vim.cmd.lclose()
-              clear_conflict_markers_autocmd(bufnr)
-              return
+              if id == qflist.id then
+                vim.cmd.lclose()
+                vim.fn.setloclist(0, {}, "u", { items = {}, title = title })
+              end
             end
-            update_context(vim.b.changedtick)
           end
-        end
-      end,
-    })
 
-    update_context(vim.b.changedtick)
-    vim.cmd.lopen()
-  else
-    vim.notify("No conflict markers found.")
+          if obj.code == 0 then
+            id = qfid
+            local loclist = vim.fn.getloclist(0, { context = 1, id = id })
+            local context = vim.tbl_extend(
+              "keep",
+              { conflict_markers = { changedtick = vim.b[bufnr].changedtick } },
+              loclist.context
+            )
+
+            vim.fn.setloclist(0, {}, "a", { context = context, id = id })
+          elseif obj.code == 1 then
+            cleanup()
+            vim.notify("No conflict markers found.")
+          elseif obj.code > 1 then
+            cleanup()
+            vim.notify(string.format(
+              "Error searching for “%s”. Unmatched quotes? Check your command.", args
+            ))
+          end
+        end,
+        title = title,
+      }
+    )
   end
+
+  update_conflict_markers()
+  clear_conflict_markers_autocmd()
+
+  vim.api.nvim_create_augroup("conflict_markers", { clear = false })
+
+  conflict_marker_autocmd = vim.api.nvim_create_autocmd({ "BufWritePost", "WinEnter" }, {
+    group = conflict_markers,
+    buffer = bufnr,
+    callback = function(args)
+      local bufnr = args.buf
+
+      if vim.api.nvim_get_current_buf() ~= bufnr then
+        -- After a BufWritePost do nothing if bufnr is not current
+        return
+      end
+
+      if not id then
+        -- Job has not exited, no loclist id, no context
+        return
+      end
+
+      if vim.fn.getloclist(0, { id = 0 }).id == id then
+        local loclist = vim.fn.getloclist(0, { context = 1, id = 0 })
+
+        if loclist.context.conflict_markers.changedtick < vim.b.changedtick then
+          update_conflict_markers()
+        end
+      end
+    end,
+  })
 end, { nargs = 0 })
--- lua/lbrayner/ripgrep.lua
+++ lua/lbrayner/ripgrep.lua
@@ -1,71 +1,166 @@
+-- vim: fdm=marker
 -- From vim-ripgrep (https://github.com/lbrayner/vim-ripgrep)
 
 local M = {}
 
-local function rg(txt, config_path, loclist)
-  local command = "grep!"
-  if loclist then
-    command = "lgrep!"
-  end
-  local rgopts = " "
-  if vim.o.ignorecase then
-    rgopts = rgopts.."-i "
-  end
-  if vim.o.smartcase then
-    rgopts = rgopts.."-S "
-  end
-  if not vim.startswith(vim.o.grepprg, "rg") and
-    not string.find(vim.o.grepprg, "^RIPGREP_CONFIG_PATH=.* rg") then
-    error("Rg: 'grepprg' not correctly set.")
-  end
-  if not vim.fn.executable("rg") then
+local function join(t) -- {{{
+  assert(type(t) == "table", "'t' must be a table")
+  return table.concat(t, " ")
+end -- }}}
+
+local function rg(args, opts) -- {{{
+  if vim.fn.executable("rg") == 0 then
     error("Rg: 'rg' not executable.")
   end
 
-  -- Escaping Command-line special characters '#', '%' (:h :_%), and '|' (:h :bar)
-  local grep = "silent "..command.." "..rgopts..vim.fn.escape(txt, "#%|")
+  local grep, rgopts = "rg --engine=auto --vimgrep --sort path", {}
+  local cclose, copen, getqf, setqf
 
-  if config_path and vim.uv.fs_stat(config_path) then
-    if vim.bo.grepprg == "" then
-      vim.bo.grepprg = "RIPGREP_CONFIG_PATH=" .. config_path .. " " .. vim.go.grepprg
+  if opts.loclist then
+    cclose, copen = vim.cmd.lclose, vim.cmd.lopen
 
-      local success, err = pcall(vim.cmd, grep)
-
-      vim.bo.grepprg = ""
-
-      if not success then
-        error(err)
-      end
-
-      return
-    else
-      error("Rg: local 'grepprg' is set.")
+    getqf = function(...)
+      return vim.fn.getloclist(opts.loclist, ...)
     end
+
+    setqf = function(...)
+      return vim.fn.setloclist(opts.loclist, ...)
+    end
+  else
+    cclose, copen = vim.cmd.cclose, vim.cmd.copen
+    getqf, setqf = vim.fn.getqflist, vim.fn.setqflist
   end
 
-  vim.cmd(grep)
-end
+  if opts.config_path and vim.uv.fs_stat(opts.config_path) then
+    grep = join({ "RIPGREP_CONFIG_PATH=" .. opts.config_path, grep })
+  end
 
-function M.lrg(txt, config_path)
-  rg(txt, config_path, true)
-end
+  if vim.o.ignorecase then
+    table.insert(rgopts, "-i")
+  end
 
-function M.rg(txt, config_path)
-  rg(txt, config_path)
+  if vim.o.smartcase then
+    table.insert(rgopts, "-S")
+  end
+
+  local cmd, qfid = join({ grep, join(rgopts), args })
+  local title = opts.title or cmd
+
+  vim.system(
+    { "sh", "-c", cmd },
+    {
+      cwd = vim.fn.getcwd(),
+      stdout = vim.schedule_wrap(function(err, data)
+        assert(not err, err)
+
+        local qflist
+
+        if not data then
+          if qfid then
+            setqf({}, "a", { id = qfid, title = title })
+            qflist = getqf({ id = 0 })
+
+            if qfid == qflist.id then copen() end
+          end
+
+          return
+        end
+
+        local lines = vim.split(data, "\n")
+        local last = lines[#lines]
+
+        if last == "" then
+          table.remove(lines) -- Pop the top
+        end
+
+        local action = " "
+        qflist = getqf({ id = qfid, title = 1 })
+
+        if qfid and qfid == qflist.id then
+          action = "a"
+        elseif title == qflist.title then
+          action = "u"
+        end
+
+        setqf({}, action, {
+          efm = "%f:%l:%c:%m",
+          context = { ripgrep = { args = args } },
+          lines = lines,
+          title = join({ title, "" }),
+        })
+
+        if not qfid then
+          qflist = getqf({ id = 0 })
+          qfid = qflist.id
+        end
+      end),
+      text = true,
+    },
+    vim.schedule_wrap(function(obj)
+      if opts.on_exit then
+        opts.on_exit(obj, args, qfid)
+      elseif obj.code == 1 then
+        vim.notify(string.format("No match found for “%s”.", args))
+      elseif obj.code > 1 then
+        if qfid then
+          qflist = getqf({ id = 0 })
+
+          if qfid == qflist.id then cclose() end
+        end
+
+        vim.notify(string.format(
+          "Error searching for “%s”. Unmatched quotes? Check your command.", args
+        ))
+        -- else add to title (ERROR)
+      end
+    end)
+  )
+end -- }}}
+
+function M.rg(args, opts)
+  assert(type(args) == "string", "'args' must be a string")
+  vim.validate("opts", opts, function(opts)
+    if type(opts) ~= "table" then
+      return false, "'opts' must be a table"
+    end
+
+    if opts.config_path and type(opts.config_path) ~= "string" then
+      return false, "'config_path' must be a string"
+    end
+
+    if opts.loclist and type(opts.loclist) ~= "number" then
+      return false, "'loclist' must be a number (winid)"
+    end
+
+    if opts.on_exit and type(opts.on_exit) ~= "function" then
+      return false, "'on_exit' must be a function"
+    end
+
+    if opts.title and type(opts.title) ~= "string" then
+      return false, "'title' must be a string"
+    end
+
+    return true
+  end, true, "'opts' table")
+
+  opts = opts or {}
+
+  return rg(args, opts)
 end
 
 function M.user_command_with_config_path(command_name, config_path)
-  vim.api.nvim_create_user_command(command_name, function(command)
-    local txt = command.args
-    local count = command.count
-    local line1 = command.line1
-    local line2 = command.line2
+  vim.api.nvim_create_user_command(command_name, function(opts)
+    local args = opts.args
+    local count = opts.count
+    local line1 = opts.line1
+    local line2 = opts.line2
 
     if count == 0 then -- :0Rg
       local context = vim.fn.getqflist({ context = 1 }).context
-      if vim.tbl_get(context, "ripgrep", "txt") then
+
+      if vim.tbl_get(context, "ripgrep", "args") then
         -- :0Rg performs a search with the last text juxtaposed with the new text
-        txt = vim.trim(table.concat({ context.ripgrep.txt, txt }, " "))
+        args = vim.trim(join({ context.ripgrep.args, args }))
       else
         vim.notify("Could not find a ripgrep search context.")
         return
@@ -74,49 +169,32 @@ function M.user_command_with_config_path(command_name, config_path)
       -- https://neovim.discourse.group/t/function-that-return-visually-selected-text/1601
       local pos_start = vim.api.nvim_buf_get_mark(0, "<")
       local pos_end = vim.api.nvim_buf_get_mark(0, ">")
+
       if line1 ~= pos_start[1] or line2 ~= pos_end[1] then
         vim.notify("Line range not allowed, only visual selection.")
         return
       end
+
       if pos_start[1] ~= pos_end[1] then
         vim.notify("Visual selection pattern cannot span multiple lines.")
         return
       end
+
       local start_row = pos_start[1] - 1
       local start_col = pos_start[2]
       local end_row = pos_end[1] - 1
       local end_col = pos_end[2] + 1
       local visual_selection = vim.api.nvim_buf_get_text(0, start_row, start_col, end_row, end_col, {})[1]
-      txt = vim.trim(table.concat({
+
+      args = vim.trim(join({
         string.format("-s -F -e %s", vim.fn.shellescape(visual_selection)),
-        txt }, " "))
+        args
+      }))
     end
 
-    local success, err = pcall(M.rg, txt, config_path)
-
-    if not success then
-      vim.cmd.cclose()
-      if type(err) == "string" and require("lbrayner").contains(err, " Rg:") then
-        error(err)
-      end
-      vim.notify(string.format("Error searching for “%s”. Unmatched quotes? Check your command.", txt))
-      return
-    end
-
-    vim.fn.setqflist({}, "a", { context = { ripgrep = { txt = txt } } })
-
-    if not vim.tbl_isempty(vim.fn.getqflist()) then
-      vim.cmd("botright copen")
-    else
-      vim.cmd.cclose()
-      vim.notify(string.format("No match found for “%s”.", txt))
-    end
+    M.rg(args, { config_path = config_path })
   end, { complete = "file", nargs = "*", range = -1 })
 end
-
-vim.go.grepformat = "%f:%l:%c:%m"
-vim.go.grepprg = "rg --engine=auto --vimgrep --sort path"
-vim.go.shellpipe = "&>"
 
 M.user_command_with_config_path("Rg")
 M.user_command_with_config_path("RgNoTests", ".ripgreprc-no-tests")
@@ -124,7 +202,6 @@ M.user_command_with_config_path("RgTests", ".ripgreprc-tests")
 
 vim.keymap.set("ca", "Rb", [[Rg -s -e'\b''\b'<Left><Left><Left><Left>]])
 vim.keymap.set("ca", "Rg", "Rg -e")
-vim.keymap.set("ca", "Rt", [[RgTests -s -e'\b''\b'<Left><Left><Left><Left>]])
 vim.keymap.set("ca", "Rw", [[Rg -s -e'\b'<C-R><C-W>'\b']])
 
 return M
