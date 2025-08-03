984918e14b06c5b62778fa8599e26310419fc269
lbrayner <brayner.l@gmail.com>
2025-08-03 17:14:27 -0300

    Code review: localization, join, concat
    For maximum efficiency.

--- lua/lbrayner/statusline.lua
+++ lua/lbrayner/statusline.lua
@@ -3,9 +3,18 @@
 -- {{{ Helper functions
 
 local FugitiveResult = vim.fn.FugitiveResult
+local concat = table.concat
 local exists = vim.fn.exists
 local fnamemodify = vim.fn.fnamemodify
 local hlID = vim.fn.hlID
+local join = function(t) -- maximum effieciency
+  return concat(t, "")
+end
+local nvim_buf_get_name = vim.api.nvim_buf_get_name
+local nvim_create_autocmd = vim.api.nvim_create_autocmd
+local nvim_get_current_buf = vim.api.nvim_get_current_buf
+local nvim_get_hl = vim.api.nvim_get_hl
+local nvim_set_hl = vim.api.nvim_set_hl
 local pathshorten = vim.fn.pathshorten
 local synIDattr = vim.fn.synIDattr
 local synIDtrans = vim.fn.synIDtrans
@@ -19,12 +28,12 @@ local function get_fugitive_git_dir()
 end
 
 local function get_fugitive_temporary_buffer_name()
-  return "Git " .. table.concat(FugitiveResult(vim.api.nvim_get_current_buf()).args, " ")
+  return concat({ "Git", concat(FugitiveResult(nvim_get_current_buf()).args, " ")}, " ")
 end
 
 local function get_line_format()
   if vim.bo.buftype == "terminal" then
-    return "%" .. (#tostring(vim.bo.scrollback)+1) .. "l"
+    return join({ "%", (#tostring(vim.bo.scrollback)+1), "l" })
   end
 
   local length = #tostring(vim.fn.line("$"))
@@ -33,12 +42,12 @@ local function get_line_format()
     length = 5
   end
 
-  return "%" .. length .. "l"
+  return join({ "%", length, "l" })
 end
 
 local function get_number_of_lines()
   if vim.bo.buftype == "terminal" then
-    return "%" .. (#tostring(vim.bo.scrollback)+1) .. "L"
+    return join({ "%", (#tostring(vim.bo.scrollback)+1), "L" })
   end
 
   local length = #tostring(vim.fn.line("$"))
@@ -47,13 +56,13 @@ local function get_number_of_lines()
     length = 5
   end
 
-  return "%-" .. length .. "L"
+  return join({ "%-", length, "L" })
 end
 
 -- }}}
 
 local function get_buffer_position() -- {{{
-  return get_line_format() .. ",%-3.v %3.P " .. get_number_of_lines()
+  return join({ get_line_format(), ",%-3.v %3.P ", get_number_of_lines() })
 end -- }}}
 
 local M = {}
@@ -64,7 +73,7 @@ function M.get_buffer_name(opts)
 
   if require("lbrayner.fugitive").get_fugitive_object() then
     path = require("lbrayner.fugitive").get_fugitive_object()
-  elseif vim.startswith(vim.api.nvim_buf_get_name(0), "jdt://") then -- jdtls
+  elseif vim.startswith(nvim_buf_get_name(0), "jdt://") then -- jdtls
     path = require("lbrayner.jdtls").get_buffer_name(0)
   else
     path = require("lbrayner.path").path()
@@ -77,7 +86,7 @@ function M.get_buffer_name(opts)
   end
 
   if buffer_name == "" then
-    return "#" .. vim.api.nvim_get_current_buf()
+    return join({ "#", nvim_get_current_buf() })
   end
 
   return buffer_name
@@ -87,22 +96,22 @@ function M.get_buffer_status()
   local status = vim.bo.modified and "%1*" or ""
 
   if vim.wo.previewwindow then
-    status = status .. "%<" .. pathshorten(require("lbrayner.path").full_path())
-  elseif require("lbrayner").buf_is_scratch() and vim.api.nvim_buf_get_name(0) == "" then
-    status = status .. "%<%5*%f%*"
+    status = join({ status, "%<", pathshorten(require("lbrayner.path").full_path()) })
+  elseif require("lbrayner").buf_is_scratch() and nvim_buf_get_name(0) == "" then
+    status = join({ status, "%<%5*%f%*" })
   elseif vim.bo.buftype ~= "" then
-    status = status .. "%<%5*" .. M.get_buffer_name({ tail = true }) .. "%*"
+    status = join({ status, "%<%5*", M.get_buffer_name({ tail = true }), "%*" })
   else
-    status = status .. "%<" .. M.get_buffer_name({ tail = true })
+    status = join({ status, "%<", M.get_buffer_name({ tail = true }) })
   end
 
-  status = status .. (vim.bo.modified and " " or " %1*") .. M.get_status_flag() .. "%*"
+  status = join({ status, (vim.bo.modified and " " or " %1*"), M.get_status_flag(), "%*" })
 
   return status
 end
 
 function M.get_diagnostics()
-  local bufnr = vim.api.nvim_get_current_buf()
+  local bufnr = nvim_get_current_buf()
 
   if vim.tbl_isempty(vim.diagnostic.get(bufnr)) then
     return " "
@@ -116,11 +125,11 @@ function M.get_empty()
 end
 
 function M.get_minor_modes()
-  local bufnr = vim.api.nvim_get_current_buf()
+  local bufnr = nvim_get_current_buf()
   local modes = vim.tbl_get(vim.b[bufnr], "lbrayner", "statusline", "modes", "str")
 
   if modes and modes ~= "" then
-    return "%9*" .. modes .. "%* "
+    return join({ "%9*", modes, "%* " })
   end
 
   return ""
@@ -152,49 +161,67 @@ end
 -- margins of 1 column (on both sides)
 function M.get_statusline()
   if require("lbrayner.fugitive").is_fugitive_blame() then
-    return " Fugitive blame " ..
-    "%<%1*%{v:lua.require'lbrayner.statusline'.get_status_flag()}%*%=" .. get_buffer_position()
+    return join({
+      " Fugitive blame ",
+      "%<%1*%{v:lua.require'lbrayner.statusline'.get_status_flag()}%*%=",
+      get_buffer_position()
+    })
   end
 
   local leftline = " "
   if vim.wo.previewwindow then
-    leftline = leftline .. "%5*%w%* "
+    leftline = join({ leftline, "%5*%w%* " })
   end
 
   if vim.b.fugitive_type and vim.b.fugitive_type == "index" then -- Fugitive summary
     local dir = pathshorten(get_fugitive_git_dir())
-    leftline = leftline .. "%6*" .. dir .. "$%* %<" .. "Fugitive summary " ..
-    "%1*%{v:lua.require'lbrayner.statusline'.get_status_flag()}%*"
+    leftline = join({
+      leftline, "%6*", dir, "$%* %<", "Fugitive summary ",
+      "%1*%{v:lua.require'lbrayner.statusline'.get_status_flag()}%*"
+    })
   elseif exists("*FugitiveResult") == 1 and
-    not vim.tbl_isempty(FugitiveResult(vim.api.nvim_get_current_buf())) then -- Fugitive temporary buffers
+    not vim.tbl_isempty(FugitiveResult(nvim_get_current_buf())) then -- Fugitive temporary buffers
     local fugitive_temp_buf = get_fugitive_temporary_buffer_name()
     local dir = pathshorten(get_fugitive_git_dir())
-    leftline = leftline .. "%6*" .. dir .. "$%* %<" .. fugitive_temp_buf ..
-    " %1*%{v:lua.require'lbrayner.statusline'.get_status_flag()}%*"
+    leftline = join({
+      leftline, "%6*", dir, "$%* %<", fugitive_temp_buf,
+      " %1*%{v:lua.require'lbrayner.statusline'.get_status_flag()}%*"
+    })
   elseif require("lbrayner").is_quickfix_or_location_list() then
-    leftline = leftline ..
-    "%<%5*%f%* %{v:lua.require'lbrayner'.get_quickfix_or_location_list_title()}"
+    leftline = join({
+      leftline,
+      "%<%5*%f%* %{v:lua.require'lbrayner'.get_quickfix_or_location_list_title()}"
+    })
   elseif vim.w.cmdline then
-    leftline = leftline .. "%<%5*[Command Line]%*"
+    leftline = join({ leftline, "%<%5*[Command Line]%*" })
   else
-    leftline = leftline .. "%{%v:lua.require'lbrayner.statusline'.get_buffer_status()%}"
+    leftline = join({
+      leftline, "%{%v:lua.require'lbrayner.statusline'.get_buffer_status()%}"
+    })
   end
 
-  local rightline = "%{%v:lua.require'lbrayner.statusline'.get_minor_modes()%}" .. get_buffer_position()
+  local rightline = join({
+    "%{%v:lua.require'lbrayner.statusline'.get_minor_modes()%}",
+    get_buffer_position()
+  })
 
   if vim.bo.buftype ~= "" then
-    rightline = rightline ..
-    "%( %6*%{v:lua.require'lbrayner.statusline'.get_version_control()}%*%) %2*%{&filetype}%* "
+    rightline = join({
+      rightline,
+      "%( %6*%{v:lua.require'lbrayner.statusline'.get_version_control()}%*%) %2*%{&filetype}%* "
+    })
   else
-    rightline = rightline ..
-    " %{%v:lua.require'lbrayner.statusline'.get_diagnostics()%}" ..
-    "%( %6*%{v:lua.require'lbrayner.statusline'.get_version_control()}%*%)" ..
-    " %4*%{v:lua.require'lbrayner'.options(&fileencoding, &encoding, '')}%*" ..
-    " %4.(%4*%{&fileformat}%*%)" ..
-    " %2*%{&filetype}%* "
+    rightline = join({
+      rightline,
+      " %{%v:lua.require'lbrayner.statusline'.get_diagnostics()%}",
+      "%( %6*%{v:lua.require'lbrayner.statusline'.get_version_control()}%*%)",
+      " %4*%{v:lua.require'lbrayner'.options(&fileencoding, &encoding, '')}%*",
+      " %4.(%4*%{&fileformat}%*%)",
+      " %2*%{&filetype}%* "
+    })
   end
 
-  return leftline .. " %=" .. rightline
+  return join({ leftline, " %=", rightline })
 end
 
 function M.get_version_control()
@@ -213,7 +240,7 @@ function M.get_version_control()
   end
 
   if string.len(branch) > 60 then
-    return string.sub(branch, 1, 54) .. "…" .. string.sub(branch, -5)
+    return join({ string.sub(branch, 1, 54), "…", string.sub(branch, -5) })
   end
 
   return branch
@@ -226,33 +253,43 @@ function M.get_winbar()
 
   local statusline = " "
   if vim.wo.previewwindow then
-    statusline = statusline .. "%w "
+    statusline = join({ statusline, "%w " })
   end
 
   if vim.b.fugitive_type and vim.b.fugitive_type == "index" then -- Fugitive summary
     local dir = pathshorten(get_fugitive_git_dir())
-    statusline = statusline .. dir .. "$ %<" .. "Fugitive summary " ..
-    "%{v:lua.require'lbrayner.statusline'.get_status_flag()}"
+    statusline = join({
+      statusline, dir, "$ %<", "Fugitive summary ",
+      "%{v:lua.require'lbrayner.statusline'.get_status_flag()}"
+    })
   elseif exists("*FugitiveResult") == 1 and
-    not vim.tbl_isempty(FugitiveResult(vim.api.nvim_get_current_buf())) then -- Fugitive temporary buffers
+    not vim.tbl_isempty(FugitiveResult(nvim_get_current_buf())) then -- Fugitive temporary buffers
     local fugitive_temp_buf = get_fugitive_temporary_buffer_name()
     local dir = pathshorten(get_fugitive_git_dir())
-    statusline = statusline .. dir .. "$ %<" .. fugitive_temp_buf ..
-    " %{v:lua.require'lbrayner.statusline'.get_status_flag()}"
+    statusline = join({
+      statusline, dir, "$ %<", fugitive_temp_buf,
+      " %{v:lua.require'lbrayner.statusline'.get_status_flag()}"
+    })
   elseif require("lbrayner").is_quickfix_or_location_list() then
-    statusline = statusline .. "%<%f %{v:lua.require'lbrayner'.get_quickfix_or_location_list_title()}"
+    statusline = join({
+      statusline, "%<%f %{v:lua.require'lbrayner'.get_quickfix_or_location_list_title()}"
+    })
   elseif vim.w.cmdline then
     statusline = ""
   else
     if vim.wo.previewwindow then
-      statusline = statusline .. "%<" .. pathshorten(require("lbrayner.path").full_path())
+      statusline = join({
+        statusline, "%<", pathshorten(require("lbrayner.path").full_path())
+      })
     else
       -- margins of 1 column, space and status flag
-      statusline = statusline ..
-      "%<%{v:lua.require'lbrayner'.truncate_filename(" ..
-      "v:lua.require'lbrayner.statusline'.get_buffer_name(), winwidth('%') - 4)}"
+      statusline = join({
+        statusline,
+        "%<%{v:lua.require'lbrayner'.truncate_filename(",
+        "v:lua.require'lbrayner.statusline'.get_buffer_name(), winwidth('%') - 4)}"
+      })
     end
-    statusline = statusline .. " %{v:lua.require'lbrayner.statusline'.get_status_flag()}"
+    statusline = join({ statusline, " %{v:lua.require'lbrayner.statusline'.get_status_flag()}" })
   end
 
   return statusline
@@ -263,23 +300,23 @@ local mapping
 function M.highlight_mode(mode)
   local hl_map_by_group = mapping[mode]
   for group, hl_map in pairs(hl_map_by_group) do
-    current_hl_map = vim.api.nvim_get_hl(0, { name = group })
+    current_hl_map = nvim_get_hl(0, { name = group })
     hl_map = vim.tbl_deep_extend("keep", { bold = true }, hl_map, {
       bg = current_hl_map.bg,
       fg = current_hl_map.fg
     })
-    vim.api.nvim_set_hl(0, group, hl_map)
+    nvim_set_hl(0, group, hl_map)
   end
 end
 
 function M.highlight_winbar()
-  local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
-  vim.api.nvim_set_hl(0, "WinBar", { bold = true, fg = normal.fg })
-  vim.api.nvim_set_hl(0, "WinBarNC", { bold = true, fg = normal.fg })
+  local normal = nvim_get_hl(0, { name = "Normal" })
+  nvim_set_hl(0, "WinBar", { bold = true, fg = normal.fg })
+  nvim_set_hl(0, "WinBarNC", { bold = true, fg = normal.fg })
 end
 
 function M.load_theme(name)
-  local success, theme = pcall(require, "lbrayner.statusline.themes." .. name)
+  local success, theme = pcall(require, join({ "lbrayner.statusline.themes.", name }))
   if not success then
     theme = require("lbrayner.statusline.themes.neosolarized")
   end
@@ -298,7 +335,7 @@ end
 function M.set_minor_modes(bufnr, mode, action)
   assert(type(bufnr) == "number" and bufnr > 0, "'bufnr' must be a positive number")
   assert(type(mode) == "string", "'mode' must be a string")
-  assert(action == "append" or action == "remove", "invalid 'action': " .. tostring(action))
+  assert(action == "append" or action == "remove", join({ "invalid 'action': ", tostring(action) }))
 
   local lbrayner = vim.b[bufnr].lbrayner or vim.empty_dict()
   local data = vim.tbl_get(lbrayner, "statusline", "modes", "data") or vim.empty_dict()
@@ -350,7 +387,7 @@ end -- }}}
 
 local statusline = vim.api.nvim_create_augroup("statusline", { clear = true })
 
-vim.api.nvim_create_autocmd("CmdlineEnter", {
+nvim_create_autocmd("CmdlineEnter", {
   pattern = { ":", "/", "?" },
   group = statusline,
   desc = "Command-line modes statusline highlight",
@@ -374,7 +411,7 @@ vim.api.nvim_create_autocmd("CmdlineEnter", {
   end,
 })
 
-vim.api.nvim_create_autocmd("ColorScheme", {
+nvim_create_autocmd("ColorScheme", {
   group = statusline,
   desc = "Load statusline theme",
   callback = function(args)
@@ -383,7 +420,7 @@ vim.api.nvim_create_autocmd("ColorScheme", {
   end,
 })
 
-vim.api.nvim_create_autocmd("ModeChanged", {
+nvim_create_autocmd("ModeChanged", {
   pattern = [[[^i]*:i*]],
   group = statusline,
   desc = "Insert mode statusline highlight",
@@ -392,7 +429,7 @@ vim.api.nvim_create_autocmd("ModeChanged", {
   end,
 })
 
-vim.api.nvim_create_autocmd("ModeChanged", {
+nvim_create_autocmd("ModeChanged", {
   pattern = [[[^vV\x16]:[vV\x16]*]],
   group = statusline,
   desc = "Visual modes statusline highlight",
@@ -401,7 +438,7 @@ vim.api.nvim_create_autocmd("ModeChanged", {
   end,
 })
 
-vim.api.nvim_create_autocmd("ModeChanged", {
+nvim_create_autocmd("ModeChanged", {
   pattern = "[^n]*:n*",
   group = statusline,
   desc = "Command/Normal mode statusline highlight",
@@ -410,7 +447,7 @@ vim.api.nvim_create_autocmd("ModeChanged", {
   end,
 })
 
-vim.api.nvim_create_autocmd("TermEnter", {
+nvim_create_autocmd("TermEnter", {
   group = statusline,
   desc = "Terminal mode statusline definition and highlight",
   callback = function()
@@ -426,13 +463,13 @@ vim.api.nvim_create_autocmd("TermEnter", {
   end,
 })
 
-vim.api.nvim_create_autocmd("TermLeave", {
+nvim_create_autocmd("TermLeave", {
   group = statusline,
   desc = "Restore regular statusline",
   callback = define_status_line,
 })
 
-vim.api.nvim_create_autocmd("VimEnter", {
+nvim_create_autocmd("VimEnter", {
   group = statusline,
   desc = "Create statusline autocmds",
   callback = function()
@@ -452,23 +489,25 @@ vim.api.nvim_create_autocmd("VimEnter", {
           end
         end)(bufnr)
 
-        local user7 = vim.api.nvim_get_hl(0, { name = "User7" })
+        local user7 = nvim_get_hl(0, { name = "User7" })
 
         if not severity then
-          vim.api.nvim_set_hl(0, "User7", vim.tbl_deep_extend("keep", { fg = "NONE" }, user7))
+          nvim_set_hl(0, "User7", vim.tbl_deep_extend("keep", { fg = "NONE" }, user7))
           return
         end
 
-        local group = "Diagnostic" .. string.sub(severity, 1, 1) .. string.lower(string.sub(severity, 2))
-        local severity_hl = vim.api.nvim_get_hl(0, { name = group })
-        vim.api.nvim_set_hl(0, "User7", vim.tbl_deep_extend("keep", { fg = severity_hl.fg }, user7))
+        local group = join({
+          "Diagnostic", string.sub(severity, 1, 1), string.lower(string.sub(severity, 2))
+        })
+        local severity_hl = nvim_get_hl(0, { name = group })
+        nvim_set_hl(0, "User7", vim.tbl_deep_extend("keep", { fg = severity_hl.fg }, user7))
       end
 
       highlight_severity(bufnr)
 
       pcall(vim.api.nvim_del_autocmd, diagnostic_changed_autocmd)
 
-      diagnostic_changed_autocmd = vim.api.nvim_create_autocmd("DiagnosticChanged", {
+      diagnostic_changed_autocmd = nvim_create_autocmd("DiagnosticChanged", {
         group = statusline,
         buffer = bufnr,
         desc = "Diagnostic severity statusline highlight",
@@ -479,7 +518,7 @@ vim.api.nvim_create_autocmd("VimEnter", {
       })
     end
 
-    vim.api.nvim_create_autocmd({ "BufWinEnter", "WinEnter" }, {
+    nvim_create_autocmd({ "BufWinEnter", "WinEnter" }, {
       group = statusline,
       desc = "Define window local statusline, buffer-local diagnostic autocmd",
       callback = function(args)
@@ -515,7 +554,7 @@ vim.api.nvim_create_autocmd("VimEnter", {
 
     vim.schedule(define_status_line)
     vim.schedule(function()
-      diagnostic_changed(vim.api.nvim_get_current_buf())
+      diagnostic_changed(nvim_get_current_buf())
     end)
   end,
 })
