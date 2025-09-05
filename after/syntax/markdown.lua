742e87bcaab7ba2ba8968f6d0545344cea1ad195
lbrayner <brayner.l@gmail.com>
2025-09-05 07:24:59 -0300

    Using nvim-treesitter (site configured)

--- after/syntax/markdown.lua
+++
@@ -1,7 +0,0 @@
-if vim.b.current_syntax ~= "markdown" then return end
-
-vim.api.nvim_set_hl(0, "markdownH2", { link = "Type" })
-vim.api.nvim_set_hl(0, "markdownH3", { link = "Underlined" })
-vim.api.nvim_set_hl(0, "markdownH4", { link = "Identifier" })
-vim.api.nvim_set_hl(0, "markdownH5", { link = "Statement" })
-vim.api.nvim_set_hl(0, "markdownH6", { link = "Constant" })
