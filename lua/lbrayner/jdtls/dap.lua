0009c2f6886979a32be068f1d3cb6d5228618aa1
lbrayner <brayner.l@gmail.com>
2025-10-01 15:00:16 -0300

    Code review: short circuit, less indenting

--- lua/lbrayner/jdtls/dap.lua
+++ lua/lbrayner/jdtls/dap.lua
@@ -6,29 +6,30 @@ local M = {}
 function M.continue(opts)
   local session = require("dap").session()
 
-  if session and not session.stopped_thread_id then
-    vim.ui.select(
-      {
-        { command = "redefine_classes", description = "Hot code replace (redefineClasses)" },
-        { command = "dap_continue", description = "See more DAP options..." },
-      },
-      {
-        prompt = "A DAP session is active. What would you like to do?",
-        format_item = function(c) return c.description end,
-      },
-      function(c)
-        if not c then return end
-
-        if c.command == "redefine_classes" then
-          require("lbrayner.jdtls").java_redefine_classes()
-        else
-          require("lbrayner.dap").continue(opts)
-        end
-      end
-    )
-  else
+  if not session or session and session.stopped_thread_id then
     require("lbrayner.dap").continue(opts)
+    return
   end
+
+  vim.ui.select(
+    {
+      { command = "redefine_classes", description = "Hot code replace (redefineClasses)" },
+      { command = "dap_continue", description = "See more DAP options..." },
+    },
+    {
+      prompt = "A DAP session is active. What would you like to do?",
+      format_item = function(c) return c.description end,
+    },
+    function(c)
+      if not c then return end
+
+      if c.command == "redefine_classes" then
+        require("lbrayner.jdtls").java_redefine_classes()
+      else
+        require("lbrayner.dap").continue(opts)
+      end
+    end
+  )
 end
 
 return M
