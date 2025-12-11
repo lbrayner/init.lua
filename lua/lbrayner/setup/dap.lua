1dc73c1840a6425b6017a0a6f216deea9662d412
lbrayner <brayner.l@gmail.com>
2025-12-11 11:35:38 -0300

    DAP: shotcurts ClearBreakpoints, StepInto, ToggleBreakpoint

--- lua/lbrayner/setup/dap.lua
+++ lua/lbrayner/setup/dap.lua
@@ -15,10 +15,22 @@ if vim.v.vim_did_enter == 1 then
   vim.api.nvim_exec_autocmds("VimEnter", { group = dap_custom })
 end
 
+vim.api.nvim_create_user_command("ClearBreakpoints", function()
+  require("dap").clear_breakpoints()
+end, { nargs = 0 })
+
 vim.api.nvim_create_user_command("DapListBreakpoints", function()
   require("dap").list_breakpoints(true) -- openqf
 end, { nargs = 0 })
 
 vim.api.nvim_create_user_command("DapTerminateAll", function()
   require("dap").terminate({ all = true })
+end, { nargs = 0 })
+
+vim.api.nvim_create_user_command("StepInto", function()
+  require("dap").step_into()
+end, { nargs = 0 })
+
+vim.api.nvim_create_user_command("ToggleBreakpoint", function()
+  require("dap").toggle_breakpoint()
 end, { nargs = 0 })
