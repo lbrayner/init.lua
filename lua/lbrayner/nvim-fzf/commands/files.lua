df2630d4a34b2c3fd5f0fbed5718496f30bba499
lbrayner <brayner.l@gmail.com>
2026-01-15 15:04:01 -0300

    Actions

--- lua/lbrayner/nvim-fzf/commands/files.lua
+++ lua/lbrayner/nvim-fzf/commands/files.lua
@@ -1,16 +1,23 @@
 -- vim: fdm=marker
 
-local fzf = require "fzf".fzf
 local concat = table.concat
+local fnameescape = vim.fn.fnameescape
+local fzf = require("fzf").fzf
 local shellescape = vim.fn.shellescape
+local utils = require("fzf-commands.utils")
 
-local utils = require "fzf-commands.utils"
+local SPLIT      = "ctrl-s"
+local TAB        = "ctrl-t"
+local TAB_BEFORE = "alt-t"
+local VSPLIT     = "alt-s"
 
-local fn, api = utils.helpers()
-
-local function file_jump(selected) -- {{{
+local function jump(selected) -- {{{
   if #selected > 2 then
-    vim.notify("Cannot jump to multiple files.", vim.log.levels.WARN)
+    -- vim.schedule(function()
+    --   vim.api.nvim_echo({ { "[FZF files] Cannot jump to multiple files", "Normal" } }, true, {})
+    -- end)
+    -- vim.notify("[FZF files] Cannot jump to multiple files", vim.log.levels.WARN)
+    -- vim.cmd.redraw()
     return
   else
     local bufnr = vim.fn.bufadd(selected[2])
@@ -18,7 +25,7 @@ local function file_jump(selected) -- {{{
   end
 end -- }}}
 
-local function file_tabedit_before(selected) -- {{{
+local function tabedit_before(selected) -- {{{
   for i=2, #selected do
     local bufnr = vim.fn.bufadd(selected[i])
     -- from fzf-lua's actions (vimcmd_entry)
@@ -26,50 +33,39 @@ local function file_tabedit_before(selected) -- {{{
   end
 end -- }}}
 
-local function files(opts)
+return function (opts)
   opts = utils.normalize_opts(opts)
-  local executable
-  if fn.executable("fd") == 1 then
-     executable = "fd"
-  else
-     -- tail to get rid of current directory from the results
-     executable = "find"
-  end
-
   local command = "rg --files --sort path"
-
   local fzf_cli_args = concat({
-    opts.fzf_cli_args or "",
-    " --ansi --multi --expect=",
-    shellescape("alt-s,alt-t,ctrl-s,ctrl-t,ctrl-]")
+    "--ansi --multi --expect=",
+    shellescape(concat({ SPLIT, TAB, TAB_BEFORE, VSPLIT }, ",")),
+    opts.fzf_cli_args and concat({ " ", opts.fzf_cli_args }) or "",
   })
-  print("fzf_cli_args", fzf_cli_args) -- TODO debug
 
   coroutine.wrap(function ()
     local selected = opts.fzf(command, fzf_cli_args)
 
     if not selected then return end
 
-    local key = selected[1]
+    local action = selected[1]
+    local vicmd
 
-    local vimcmd
-    if key == "alt-t" then
-      file_tabedit_before(selected)
+    if action == SPLIT then
+      vicmd = "new"
+    elseif action == TAB then
+      vicmd = "tabnew"
+    elseif action == TAB_BEFORE then
+      tabedit_before(selected)
       return
-    elseif key == "alt-s" then
-      vimcmd = "vnew"
-    elseif key == "ctrl-s" then
-      vimcmd = "new"
+    elseif action == VSPLIT then
+      vicmd = "vnew"
     else
-      file_jump(selected)
+      jump(selected)
       return
     end
 
-    for i=2,#selected do
-      vim.cmd(vimcmd .. " " .. fn.fnameescape(selected[i]))
+    for i=2, #selected do
+      vim.cmd(concat({ vicmd, fnameescape(selected[i]) }, " "))
     end
-
   end)()
 end
-
-return files
