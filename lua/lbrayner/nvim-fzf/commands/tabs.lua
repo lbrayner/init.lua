f682ca29ad5fad7b10f8a4c570dc25dfcf71d88c
lbrayner <brayner.l@gmail.com>
2026-01-16 16:48:10 -0300

    strip_cwd uses nvim_win_call, Tab entry back

--- lua/lbrayner/nvim-fzf/commands/tabs.lua
+++ lua/lbrayner/nvim-fzf/commands/tabs.lua
@@ -3,24 +3,22 @@
 local concat = table.concat
 local fnamemodify = vim.fn.fnamemodify
 local get_history_file = require("lbrayner.nvim-fzf.history").get_history_file
+local getbufinfo = vim.fn.getbufinfo
 local getcwd = vim.fn.getcwd
+local nvim_win_call = vim.api.nvim_win_call
 local sanitize_history_file = require("lbrayner.nvim-fzf.history").sanitize_history_file
 local shellescape = vim.fn.shellescape
-local startswith = vim.startswith
 
-local function strip_cwd(tabnr, name) -- {{{
-  -- print("tabnr", vim.inspect(tabnr), "name", vim.inspect(name)) -- TODO debug
-  local cwd = getcwd(-1, tabnr)
-  -- print("cwd", vim.inspect(cwd), "name", vim.inspect(name)) -- TODO debug
-  name = fnamemodify(name, ":p")
-  -- print("name", vim.inspect(name)) -- TODO debug
+  local TAB = ( -- {{{
+      "	")
+      -- }}}
 
-  if not startswith(name, cwd) then
-    return fnamemodify(name, ":~")
-  end
+local function strip_cwd(winid, name) -- {{{
+  nvim_win_call(winid, function()
+    name = fnamemodify(name, ":~:.")
+  end)
 
-  -- print("name", vim.inspect(name), "cwd", vim.inspect(cwd)) -- TODO debug
-  return name:sub(#cwd + 2)
+  return name
 end -- }}}
 
 return function (opts)
@@ -28,10 +26,16 @@ return function (opts)
   local entries = {}
 
   for tabnr, tabh in ipairs(vim.api.nvim_list_tabpages()) do
+    local cwd = getcwd(-1, tabnr)
+    table.insert(
+      entries,
+      concat({ tabnr, fnamemodify(cwd, ":~") }, TAB)
+    )
+
     for _, w in ipairs(vim.api.nvim_tabpage_list_wins(tabh)) do
       -- From fzf-lua.providers.buffers's gen_buffer_entry
       local bufnr = vim.api.nvim_win_get_buf(w)
-      local info = vim.fn.getbufinfo(bufnr)[1]
+      local info = getbufinfo(bufnr)[1]
       local hidden = info.hidden == 1 and "h" or "a"
       local readonly = vim.bo[bufnr].readonly and "=" or " "
       local changed = info.changed == 1 and "+" or " "
@@ -42,7 +46,7 @@ return function (opts)
       table.insert(
         entries,
         ("%d	%d	[%d]	%s	%s"):format(
-          tabnr, w, bufnr, flags, strip_cwd(tabnr, info.name)
+          tabnr, w, bufnr, flags, strip_cwd(w, info.name)
         )
       )
     end
