3e3801a6c1f51c8cb7e8971ed241e3f84c5f2334
lbrayner <brayner.l@gmail.com>
2026-09-14 14:35:50 -0300

    LSP: reuse_win not in vim.lsp.ListOpts

--- lua/lbrayner/lsp/_operations.lua
+++ lua/lbrayner/lsp/_operations.lua
@@ -4,11 +4,11 @@ local is_test_file = require("lbrayner.lsp").is_test_file
 local on_list = require("lbrayner.lsp").on_list
 
 function M.declaration()
-  vim.lsp.buf.declaration({ on_list = on_list, reuse_win = true })
+  vim.lsp.buf.declaration({ on_list = on_list })
 end
 
 function M.definition()
-  vim.lsp.buf.definition({ on_list = on_list, reuse_win = true })
+  vim.lsp.buf.definition({ on_list = on_list })
 end
 
 function M.document_symbol(client, cb, bufnr)
@@ -31,9 +31,8 @@ function M.hover()
   vim.lsp.buf.hover({ close_events = require("lbrayner").get_close_events() })
 end
 
--- Documentation is missing reuse_win
 function M.implementation()
-  vim.lsp.buf.implementation({ on_list = on_list, reuse_win = true })
+  vim.lsp.buf.implementation({ on_list = on_list })
 end
 
 function M.references(config)
@@ -56,7 +55,7 @@ function M.references(config)
 end
 
 function M.type_definition()
-  vim.lsp.buf.type_definition({ on_list = on_list, reuse_win = true })
+  vim.lsp.buf.type_definition({ on_list = on_list })
 end
 
 return M
